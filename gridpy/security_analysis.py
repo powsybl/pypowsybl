483cd0d30b4c0b6b425d435350c42bb2b29c21d0
Geoffroy Jamgotchian <geoffroy.jamgotchian@rte-france.com>
2021-01-05 23:43:18 +0100

    Add SecurityAnalysisResult
    Signed-off-by: Geoffroy Jamgotchian <geoffroy.jamgotchian@rte-france.com>

--- gridpy/security_analysis.py
+++ gridpy/security_analysis.py
@@ -10,12 +10,40 @@ from gridpy.util import ObjectHandle
 from typing import List
 
 
+class SecurityAnalysisResult:
+    def __init__(self, results):
+        self._post_contingency_result = {}
+        for result in results:
+            if result.contingency_id:
+                self._post_contingency_result[result.contingency_id] = result
+            else:
+                self._pre_contingency_result = result
+
+    @property
+    def pre_contingency_result(self):
+        return self._pre_contingency_result
+
+    @property
+    def post_contingency_results(self):
+        return self._post_contingency_result.values()
+
+    @property
+    def contingencies(self):
+        return self._post_contingency_result.keys()
+
+    def find_post_contingency_result(self, contingency_id: str):
+        result = self._post_contingency_result[contingency_id]
+        if not result:
+            raise Exception("Contingency '%s' not found".format(contingency_id))
+        return result
+
+
 class SecurityAnalysis(ObjectHandle):
     def __init__(self, ptr):
         ObjectHandle.__init__(self, ptr)
 
     def run(self, network: Network):
-        return _gridpy.run_security_analysis(self.ptr, network.ptr)
+        return SecurityAnalysisResult(_gridpy.run_security_analysis(self.ptr, network.ptr))
 
     def add_contingency(self, id: str, elements_ids: List[str]):
         _gridpy.add_contingency_to_security_analysis(self.ptr, id, elements_ids)
