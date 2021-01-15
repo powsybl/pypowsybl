79782ff48b866885568bbca09ece6a9accbe7461
Geoffroy Jamgotchian <geoffroy.jamgotchian@rte-france.com>
2021-01-15 22:11:45 +0100

    Split GridPyApi
    Signed-off-by: Geoffroy Jamgotchian <geoffroy.jamgotchian@rte-france.com>

---
+++ java/src/main/java/org/gridsuite/gridpy/SingleLineDiagramUtil.java
@@ -0,0 +1,48 @@
+/**
+ * Copyright (c) 2021, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package org.gridsuite.gridpy;
+
+import com.powsybl.commons.PowsyblException;
+import com.powsybl.iidm.network.Network;
+import com.powsybl.sld.NetworkGraphBuilder;
+import com.powsybl.sld.VoltageLevelDiagram;
+import com.powsybl.sld.layout.LayoutParameters;
+import com.powsybl.sld.layout.SmartVoltageLevelLayoutFactory;
+import com.powsybl.sld.layout.VoltageLevelLayoutFactory;
+import com.powsybl.sld.library.ComponentLibrary;
+import com.powsybl.sld.library.ResourcesComponentLibrary;
+import com.powsybl.sld.svg.DefaultDiagramLabelProvider;
+import com.powsybl.sld.svg.DefaultSVGWriter;
+import com.powsybl.sld.util.TopologicalStyleProvider;
+
+import java.nio.file.Paths;
+
+/**
+ * @author Geoffroy Jamgotchian {@literal <geoffroy.jamgotchian at rte-france.com>}
+ */
+public final class SingleLineDiagramUtil {
+
+    private SingleLineDiagramUtil() {
+    }
+
+    static void writeSvg(Network network, String containerId, String svgFile) {
+        ComponentLibrary componentLibrary = new ResourcesComponentLibrary("/ConvergenceLibrary");
+        if (network.getVoltageLevel(containerId) != null) {
+            VoltageLevelLayoutFactory voltageLevelLayoutFactory = new SmartVoltageLevelLayoutFactory(network);
+            VoltageLevelDiagram voltageLevelDiagram = VoltageLevelDiagram.build(new NetworkGraphBuilder(network), containerId, voltageLevelLayoutFactory, false);
+            LayoutParameters layoutParameters = new LayoutParameters()
+                    .setAdaptCellHeightToContent(true);
+            voltageLevelDiagram.writeSvg("",
+                    new DefaultSVGWriter(componentLibrary, layoutParameters),
+                    new DefaultDiagramLabelProvider(network, componentLibrary, layoutParameters),
+                    new TopologicalStyleProvider(network),
+                    Paths.get(svgFile));
+        } else {
+            throw new PowsyblException("Container '" + containerId + "' not found");
+        }
+    }
+}
