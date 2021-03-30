8905b63271bf93f40877232fefee759a323cd5ee
Geoffroy Jamgotchian <geoffroy.jamgotchian@rte-france.com>
2021-03-30 19:46:00 +0200

    Adapt to new sensi API (#59)
    Signed-off-by: Geoffroy Jamgotchian <geoffroy.jamgotchian@rte-france.com>

--- java/src/main/java/org/gridsuite/gridpy/SensitivityAnalysisContext.java
+++ java/src/main/java/org/gridsuite/gridpy/SensitivityAnalysisContext.java
@@ -11,6 +11,7 @@ import com.powsybl.computation.local.LocalComputationManager;
 import com.powsybl.contingency.Contingency;
 import com.powsybl.iidm.network.*;
 import com.powsybl.loadflow.LoadFlowParameters;
+import com.powsybl.openloadflow.sensi.*;
 import com.powsybl.sensitivity.*;
 import com.powsybl.sensitivity.factors.BranchFlowPerInjectionIncrease;
 import com.powsybl.sensitivity.factors.BranchFlowPerPSTAngle;
@@ -131,21 +132,93 @@ class SensitivityAnalysisContext extends AbstractContingencyContainer {
         return factors;
     }
 
-    SensitivityAnalysisResultContext run(Network network, LoadFlowParameters loadFlowParameters) {
+    SensitivityAnalysisResultContextV1 runV1(Network network, LoadFlowParameters loadFlowParameters) {
         SensitivityAnalysisParameters sensitivityAnalysisParameters = SensitivityAnalysisParameters.load();
         sensitivityAnalysisParameters.setLoadFlowParameters(loadFlowParameters);
         List<Contingency> contingencies = createContingencies(network);
         List<SensitivityFactor> factors = createFactors(network);
         SensitivityAnalysisResult result = SensitivityAnalysis.run(network, VariantManagerConstants.INITIAL_VARIANT_ID,
             n -> factors, contingencies, sensitivityAnalysisParameters, LocalComputationManager.getDefault());
-        SensitivityAnalysisResultContext resultContext = null;
+        SensitivityAnalysisResultContextV1 resultContext = null;
         if (result.isOk()) {
             Collection<SensitivityValue> sensitivityValues = result.getSensitivityValues();
             Map<String, List<SensitivityValue>> sensitivityValuesByContingencyId = result.getSensitivityValuesContingencies();
             int columnCount = branchsIds.size();
             int rowCount = injectionsOrTransfosIds.size();
-            resultContext = new SensitivityAnalysisResultContext(rowCount, columnCount, sensitivityValues, sensitivityValuesByContingencyId);
+            resultContext = new SensitivityAnalysisResultContextV1(rowCount, columnCount, sensitivityValues, sensitivityValuesByContingencyId);
         }
         return resultContext;
+    }
+
+    SensitivityAnalysisResultContext runV2(Network network, LoadFlowParameters loadFlowParameters) {
+        SensitivityAnalysisParameters sensitivityAnalysisParameters = SensitivityAnalysisParameters.load();
+        sensitivityAnalysisParameters.setLoadFlowParameters(loadFlowParameters);
+        List<Contingency> contingencies = createContingencies(network);
+
+        int columnCount = branchsIds != null ? branchsIds.size() : 0;
+        int rowCount = injectionsOrTransfosIds != null ? injectionsOrTransfosIds.size() : 0;
+
+        SensitivityFactorReader factorReader = handler -> {
+            if (branchsIds == null) {
+                return;
+            }
+            for (int column = 0; column < columnCount; column++) {
+                String branchId = branchsIds.get(column);
+                Branch branch = network.getBranch(branchId);
+                if (branch == null) {
+                    throw new PowsyblException("Branch '" + branchId + "' not found");
+                }
+                for (int row = 0; row < rowCount; row++) {
+                    int index = column + columnCount * row;
+                    String injectionOrTransfoId = injectionsOrTransfosIds.get(row);
+                    Injection<?> injection = getInjection(network, injectionOrTransfoId);
+                    if (injection != null) {
+                        handler.onSimpleFactor(index, SensitivityFunctionType.BRANCH_ACTIVE_POWER, branchId,
+                                SensitivityVariableType.INJECTION_ACTIVE_POWER, injectionOrTransfoId);
+                    } else {
+                        TwoWindingsTransformer twt = network.getTwoWindingsTransformer(injectionOrTransfoId);
+                        if (twt != null) {
+                            if (twt.getPhaseTapChanger() == null) {
+                                throw new PowsyblException("Transformer '" + injectionOrTransfoId + "' is not a phase shifter");
+                            }
+                            handler.onSimpleFactor(index, SensitivityFunctionType.BRANCH_ACTIVE_POWER, branchId,
+                                    SensitivityVariableType.TRANSFORMER_PHASE, injectionOrTransfoId);
+                        } else {
+                            throw new PowsyblException("Injection or transformer '" + injectionOrTransfoId + "' not found");
+                        }
+                    }
+                }
+            }
+        };
+
+        double[] baseCaseValues = new double[rowCount * columnCount];
+        double[][] valuesByContingencyIndex = new double[contingencies.size()][rowCount * columnCount];
+        double[] baseCaseReferenceFlows = new double[columnCount];
+        double[][] referenceFlowsByContingencyIndex = new double[contingencies.size()][columnCount];
+        SensitivityValueWriter valueWriter = (factorContext, contingencyId, contingencyIndex, value, functionReference) -> {
+            int factorIndex = (Integer) factorContext;
+            int columnIndex = factorIndex % columnCount;
+            if (contingencyIndex != -1) {
+                valuesByContingencyIndex[contingencyIndex][factorIndex] = value;
+                referenceFlowsByContingencyIndex[contingencyIndex][columnIndex] = functionReference;
+            } else {
+                baseCaseValues[factorIndex] = value;
+                baseCaseReferenceFlows[columnIndex] = functionReference;
+            }
+        };
+
+        new OpenSensitivityAnalysisProvider().run(network, VariantManagerConstants.INITIAL_VARIANT_ID, contingencies,
+                sensitivityAnalysisParameters, factorReader, valueWriter);
+
+        Map<String, double[]> valuesByContingencyId = new HashMap<>(contingencies.size());
+        Map<String, double[]> referenceFlowsByContingencyId = new HashMap<>(contingencies.size());
+        for (int contingencyIndex = 0; contingencyIndex < contingencies.size(); contingencyIndex++) {
+            Contingency contingency = contingencies.get(contingencyIndex);
+            valuesByContingencyId.put(contingency.getId(), valuesByContingencyIndex[contingencyIndex]);
+            referenceFlowsByContingencyId.put(contingency.getId(), referenceFlowsByContingencyIndex[contingencyIndex]);
+        }
+
+        return new SensitivityAnalysisResultContextV2(rowCount, columnCount, baseCaseValues, valuesByContingencyId,
+                baseCaseReferenceFlows, referenceFlowsByContingencyId);
     }
 }
---
+++ java/src/main/java/org/gridsuite/gridpy/SensitivityAnalysisResultContextV1.java
@@ -0,0 +1,79 @@
+/**
+ * Copyright (c) 2021, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package org.gridsuite.gridpy;
+
+import com.powsybl.sensitivity.SensitivityValue;
+import org.graalvm.nativeimage.UnmanagedMemory;
+import org.graalvm.nativeimage.c.struct.SizeOf;
+import org.graalvm.nativeimage.c.type.CDoublePointer;
+import org.graalvm.word.WordFactory;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author Geoffroy Jamgotchian {@literal <geoffroy.jamgotchian at rte-france.com>}
+ */
+class SensitivityAnalysisResultContextV1 implements SensitivityAnalysisResultContext {
+
+    private final int rowCount;
+
+    private final int columnCount;
+
+    private final Collection<SensitivityValue> sensitivityValues;
+
+    private final Map<String, List<SensitivityValue>> sensitivityValuesByContingencyId;
+
+    SensitivityAnalysisResultContextV1(int rowCount, int columnCount, Collection<SensitivityValue> sensitivityValues,
+                                       Map<String, List<SensitivityValue>> sensitivityValuesByContingencyId) {
+        this.rowCount = rowCount;
+        this.columnCount = columnCount;
+        this.sensitivityValues = sensitivityValues;
+        this.sensitivityValuesByContingencyId = sensitivityValuesByContingencyId;
+    }
+
+    private Collection<SensitivityValue> getSensitivityValues(String contingencyId) {
+        return contingencyId.isEmpty() ? sensitivityValues : sensitivityValuesByContingencyId.get(contingencyId);
+    }
+
+    @Override
+    public GridPyApiHeader.MatrixPointer createSensitivityMatrix(String contingencyId) {
+        Collection<SensitivityValue> sensitivityValues = getSensitivityValues(contingencyId);
+        if (sensitivityValues != null) {
+            CDoublePointer valuePtr = UnmanagedMemory.calloc(rowCount * columnCount * SizeOf.get(CDoublePointer.class));
+            for (SensitivityValue sensitivityValue : sensitivityValues) {
+                IndexedSensitivityFactor indexedFactor = (IndexedSensitivityFactor) sensitivityValue.getFactor();
+                valuePtr.addressOf(indexedFactor.getRow() * columnCount + indexedFactor.getColumn()).write(sensitivityValue.getValue());
+            }
+            GridPyApiHeader.MatrixPointer matrixPtr = UnmanagedMemory.calloc(SizeOf.get(GridPyApiHeader.MatrixPointer.class));
+            matrixPtr.setRowCount(rowCount);
+            matrixPtr.setColumnCount(columnCount);
+            matrixPtr.setValues(valuePtr);
+            return matrixPtr;
+        }
+        return WordFactory.nullPointer();
+    }
+
+    @Override
+    public GridPyApiHeader.MatrixPointer createReferenceFlows(String contingencyId) {
+        Collection<SensitivityValue> sensitivityValues = getSensitivityValues(contingencyId);
+        if (sensitivityValues != null) {
+            CDoublePointer valuePtr = UnmanagedMemory.calloc(columnCount * SizeOf.get(CDoublePointer.class));
+            for (SensitivityValue sensitivityValue : sensitivityValues) {
+                IndexedSensitivityFactor indexedFactor = (IndexedSensitivityFactor) sensitivityValue.getFactor();
+                valuePtr.addressOf(indexedFactor.getColumn()).write(sensitivityValue.getFunctionReference());
+            }
+            GridPyApiHeader.MatrixPointer matrixPtr = UnmanagedMemory.calloc(SizeOf.get(GridPyApiHeader.MatrixPointer.class));
+            matrixPtr.setRowCount(1);
+            matrixPtr.setColumnCount(columnCount);
+            matrixPtr.setValues(valuePtr);
+            return matrixPtr;
+        }
+        return WordFactory.nullPointer();
+    }
+}
