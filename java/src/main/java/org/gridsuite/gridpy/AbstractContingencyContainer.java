557c4873bb63a9f0dce4e9b8dba097fa71fd9ea8
Geoffroy Jamgotchian <geoffroy.jamgotchian@rte-france.com>
2021-04-07 22:53:54 +0200

    HVDC contingency support
    Signed-off-by: Geoffroy Jamgotchian <geoffroy.jamgotchian@rte-france.com>

--- java/src/main/java/org/gridsuite/gridpy/AbstractContingencyContainer.java
+++ java/src/main/java/org/gridsuite/gridpy/AbstractContingencyContainer.java
@@ -10,7 +10,9 @@ import com.powsybl.commons.PowsyblException;
 import com.powsybl.contingency.BranchContingency;
 import com.powsybl.contingency.Contingency;
 import com.powsybl.contingency.ContingencyElement;
+import com.powsybl.contingency.HvdcLineContingency;
 import com.powsybl.iidm.network.Branch;
+import com.powsybl.iidm.network.HvdcLine;
 import com.powsybl.iidm.network.Identifiable;
 import com.powsybl.iidm.network.Network;
 
@@ -39,6 +41,8 @@ abstract class AbstractContingencyContainer implements ContingencyContainer {
         }
         if (identifiable instanceof Branch) {
             return new BranchContingency(elementId);
+        } else if (identifiable instanceof HvdcLine) {
+            return new HvdcLineContingency(elementId);
         } else {
             throw new PowsyblException("Element type not supported: " + identifiable.getClass().getSimpleName());
         }
