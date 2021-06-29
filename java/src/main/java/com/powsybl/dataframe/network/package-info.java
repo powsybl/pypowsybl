ea2a2cf18567bec9267af7111c65233237302ce2
Sylvain Leclerc <sylvain.leclerc@rte-france.com>
2021-06-29 10:34:13 +0200

    Some missing license headers
    Signed-off-by: Sylvain Leclerc <sylvain.leclerc@rte-france.com>

--- java/src/main/java/com/powsybl/dataframe/network/package-info.java
+++ java/src/main/java/com/powsybl/dataframe/network/package-info.java
@@ -1,4 +1,10 @@
 /**
+ * Copyright (c) 2021, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+/**
  * Mapping of {@link com.powsybl.iidm.network.Network} elements to and from dataframes.
  *
  * @author Sylvain Leclerc <sylvain.leclerc at rte-france.com>
