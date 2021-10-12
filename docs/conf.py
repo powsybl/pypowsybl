7e1b45dc286566fd29d3b5103e30ef4cf9ce6b67
Sylvain Leclerc <sylvain.leclerc@rte-france.com>
2021-10-12 10:46:14 +0200

    Remove autodocsum
    Signed-off-by: Sylvain Leclerc <sylvain.leclerc@rte-france.com>

--- docs/conf.py
+++ docs/conf.py
@@ -32,8 +32,7 @@ extensions = ['sphinx.ext.autodoc',
               'sphinx.ext.doctest',
               'sphinx.ext.napoleon',
               'sphinx.ext.todo',
-              'sphinx.ext.intersphinx',
-              'autodocsumm']
+              'sphinx.ext.intersphinx']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
