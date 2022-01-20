ae3e3fdd85c07f4829330da43c751e3d6a669197
Sylvain Leclerc <sylvain.leclerc@rte-france.com>
2022-01-20 13:41:41 +0100

    [Build] Move native libs to pypowsyb package, remove source distribution (#291)
    Signed-off-by: Sylvain Leclerc <sylvain.leclerc@rte-france.com>

--- pypowsybl/security.py
+++ pypowsybl/security.py
@@ -9,8 +9,8 @@ from pypowsybl.util import create_data_frame_from_series_array as _create_data_f
 
 from typing import List as _List
 
-import _pypowsybl
-from _pypowsybl import ContingencyResult, LimitViolation, ContingencyContextType
+from pypowsybl import _pypowsybl
+from pypowsybl._pypowsybl import ContingencyResult, LimitViolation, ContingencyContextType
 from pypowsybl.network import Network as _Network
 from pypowsybl.util import ContingencyContainer as _ContingencyContainer
 from prettytable import PrettyTable as _PrettyTable
