44b071973471199da356dcbc85a99638590bbb9b
EtienneLt <32468651+EtienneLt@users.noreply.github.com>
2022-01-21 12:17:24 +0100

    Per unit view of the network (#193)
    Signed-off-by: Etienne Lesot <etienne.lesot@rte-france.com>
    Signed-off-by: Sylvain Leclerc <sylvain.leclerc@rte-france.com>
    Co-authored-by: Sylvain Leclerc <sylvain.leclerc@rte-france.com>

--- pypowsybl/network.py
+++ pypowsybl/network.py
@@ -9,13 +9,14 @@ from __future__ import annotations  # Necessary for type alias like _DataFrame t
 import sys as _sys
 from typing import List as _List
 from typing import Set as _Set
+
 from pypowsybl import _pypowsybl
 from pypowsybl._pypowsybl import ElementType
 
+import pandas as _pd
 from pandas import DataFrame as _DataFrame
 import networkx as _nx
 import datetime as _datetime
-import pandas as _pd
 import numpy as _np
 
 from pypowsybl.util import create_data_frame_from_series_array as _create_data_frame_from_series_array
@@ -141,6 +142,59 @@ class BusBreakerTopology:
         graph.add_nodes_from(self._buses.index.tolist())
         graph.add_edges_from(self._switchs[['bus1_id', 'bus2_id']].values.tolist())
         return graph
+
+
+def _to_array(value):
+    """
+    Converts a scalar or array to an array
+    """
+    as_array = _np.array(value, ndmin=1, copy=False)
+    if as_array.ndim != 1:
+        raise ValueError('Network elements update: expecting only scalar or 1 dimension array '
+                         'as keyword argument, got {} dimensions'.format(as_array.ndim))
+    return as_array
+
+
+def _adapt_kwargs(element_type: ElementType, **kwargs) -> _DataFrame:
+    """
+    Converts named arguments to a dataframe.
+    Element type is required to know which attributes must be part of the index.
+    """
+
+    metadata = _pypowsybl.get_series_metadata(element_type)
+    index_columns = [col.name for col in metadata if col.is_index]
+
+    columns = {}
+    expected_size = None
+    for key, value in kwargs.items():
+        col = _to_array(value)
+        size = col.shape[0]
+        if expected_size is None:
+            expected_size = size
+        elif size != expected_size:
+            raise ValueError('Network elements update: all arguments must have the same size, '
+                             'got size {} for series {}, expected {}'.format(size, key, expected_size))
+        columns[key] = col
+
+    index = None
+    if len(index_columns) == 1:
+        index_name = index_columns[0]
+        index = _pd.Index(name=index_name, data=columns[index_name])
+    elif len(index_columns) > 1:
+        index = _pd.MultiIndex.from_arrays(names=index_columns, arrays=[columns[name] for name in index_columns])
+    data = dict((k, v) for k, v in columns.items() if k not in index_columns)
+    return _DataFrame(index=index, data=data)
+
+
+def _adapt_df_or_kwargs(element_type: ElementType, df: _DataFrame, **kwargs) -> _DataFrame:
+    """
+    Ensures we get a dataframe, either from a ready to use dataframe, or from keyword arguments.
+    """
+    if df is None:
+        return _adapt_kwargs(element_type, **kwargs)
+    elif kwargs:
+        raise RuntimeError('You must provide data in only one form: dataframe or named arguments')
+    return df
 
 
 class Network(object):
@@ -1033,7 +1087,8 @@ class Network(object):
             \                 2 1.150767 0.0 0.0 0.0 0.0
             ========== ======== ======== === === === ===
         """
-        return self.get_elements(_pypowsybl.ElementType.RATIO_TAP_CHANGER_STEP)
+        ratio_tap_changer_steps = self.get_elements(_pypowsybl.ElementType.RATIO_TAP_CHANGER_STEP)
+        return ratio_tap_changer_steps
 
     def get_phase_tap_changer_steps(self) -> _DataFrame:
         """
@@ -1072,7 +1127,8 @@ class Network(object):
             ...      ...  ...    ...       ...       ... ... ...
             === ======== ==== ====== ========= ========= === ===
         """
-        return self.get_elements(_pypowsybl.ElementType.PHASE_TAP_CHANGER_STEP)
+        phase_tap_changer_steps = self.get_elements(_pypowsybl.ElementType.PHASE_TAP_CHANGER_STEP)
+        return phase_tap_changer_steps
 
     def get_ratio_tap_changers(self) -> _DataFrame:
         """
@@ -1111,7 +1167,8 @@ class Network(object):
             NHV2_NLOAD   1       0        2          3    True       True    158.0             0.0          VLLOAD_0
             ========== === ======= ======== ========== ======= ========== ======== =============== =================
         """
-        return self.get_elements(_pypowsybl.ElementType.RATIO_TAP_CHANGER)
+        ratio_tap_changers = self.get_elements(_pypowsybl.ElementType.RATIO_TAP_CHANGER)
+        return ratio_tap_changers
 
     def get_phase_tap_changers(self) -> _DataFrame:
         """
@@ -1171,61 +1228,35 @@ class Network(object):
             element_type (ElementType): the element type
             df: the data to be updated
         """
+        df = _adapt_df_or_kwargs(element_type, df, **kwargs)
+
         series_metadata = _pypowsybl.get_series_metadata(element_type)
         metadata_by_name = {s.name: s for s in series_metadata}
-
         is_index = []
         columns_names = []
         columns_values = []
         columns_types = []
-        index_count = 0
-        col_list = []
-        if df is None:
-            expected_size = None
-            for key, value in kwargs.items():
-                if not key in metadata_by_name:
-                    raise ValueError('No column named {}'.format(key))
-                columns_names.append(key)
-                metadata = metadata_by_name[key]
-                is_index.append(metadata.is_index)
-                columns_types.append(metadata.type)
-                values_array = _np.array(value, ndmin=1, copy=False)
-                if values_array.ndim != 1:
-                    raise ValueError('Network elements update: expecting only scalar or 1 dimension array '
-                                     'as keyword argument, got {} dimensions'.format(values_array.ndim))
-                size = values_array.shape[0]
-                if expected_size is None:
-                    expected_size = size
-                elif size != expected_size:
-                    raise ValueError('Network elements update: all arguments must have the same size, '
-                                     'got size {} for series {}, expected {}'.format(size, key, expected_size))
-                columns_values.append(values_array)
-                index_count += 1
-        else:
-            if kwargs:
-                raise RuntimeError('You must provide data in only one form: dataframe or named arguments')
-            is_multi_index = len(df.index.names) > 1
+        is_multi_index = len(df.index.names) > 1
 
-            for idx, index_name in enumerate(df.index.names):
-                if index_name is None:
-                    index_name = series_metadata[idx].name
-                if is_multi_index:
-                    columns_values.append(df.index.get_level_values(index_name))
-                else:
-                    columns_values.append(df.index.values)
-                columns_names.append(index_name)
-                columns_types.append(metadata_by_name[index_name].type)
-                index_count += 1
-                is_index.append(True)
-            columns_names.extend(df.columns.values)
-            for series_name in df.columns.values:
-                if not series_name in metadata_by_name:
-                    raise ValueError('No column named {}'.format(series_name))
-                series = df[series_name]
-                series_type = metadata_by_name[series_name].type
-                columns_types.append(series_type)
-                columns_values.append(series.values)
-                is_index.append(False)
+        for idx, index_name in enumerate(df.index.names):
+            if index_name is None:
+                index_name = series_metadata[idx].name
+            if is_multi_index:
+                columns_values.append(df.index.get_level_values(index_name))
+            else:
+                columns_values.append(df.index.values)
+            columns_names.append(index_name)
+            columns_types.append(metadata_by_name[index_name].type)
+            is_index.append(True)
+        columns_names.extend(df.columns.values)
+        for series_name in df.columns.values:
+            if not series_name in metadata_by_name:
+                raise ValueError('No column named {}'.format(series_name))
+            series = df[series_name]
+            series_type = metadata_by_name[series_name].type
+            columns_types.append(series_type)
+            columns_values.append(series.values)
+            is_index.append(False)
         array = _pypowsybl.create_dataframe(columns_values, columns_names, columns_types, is_index)
         _pypowsybl.update_network_elements_with_series(self._handle, array, element_type)
 
@@ -1653,6 +1684,15 @@ class Network(object):
                 In the case of sequences, all arguments must have the same length.
         """
         return self.update_elements(_pypowsybl.ElementType.NON_LINEAR_SHUNT_COMPENSATOR_SECTION, df, **kwargs)
+
+    def update_busbar_sections(self, df):
+        """Update phase tap changers with a ``Pandas`` data frame.
+
+        Args:
+            df (DataFrame): the ``Pandas`` data frame
+
+        """
+        return self.update_elements(_pypowsybl.ElementType.BUSBAR_SECTION, df)
 
     def get_working_variant_id(self):
         """
