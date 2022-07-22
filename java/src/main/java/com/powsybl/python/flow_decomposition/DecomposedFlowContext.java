15981282d0d0bc8c7a3758c235519b1001bd0626
Hugo SCHINDLER <hugo.schindler@rte-france.com>
2022-07-22 15:15:01 +0200

    add header
    Signed-off-by: Hugo SCHINDLER <hugo.schindler@rte-france.com>

--- java/src/main/java/com/powsybl/python/flow_decomposition/DecomposedFlowContext.java
+++ java/src/main/java/com/powsybl/python/flow_decomposition/DecomposedFlowContext.java
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2022, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 package com.powsybl.python.flow_decomposition;
 
 import com.powsybl.flow_decomposition.DecomposedFlow;
@@ -9,6 +15,9 @@ import java.util.Set;
 import java.util.TreeMap;
 import java.util.function.ToDoubleFunction;
 
+/**
+ * @author Hugo Schindler {@literal <hugo.schindler at rte-france.com>}
+ */
 public class DecomposedFlowContext extends DecomposedFlow {
 
     private final String xnecId;
