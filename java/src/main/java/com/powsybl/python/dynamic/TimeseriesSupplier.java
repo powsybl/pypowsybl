87273893a480c72bf57182790b410c552e19a7e0
Nicolas PIERRE <nicolas.pierre@artelys.com>
2022-10-24 10:14:16 +0200

    basic event and timeseries handler
    Signed-off-by: Nicolas PIERRE <nicolas.pierre@artelys.com>

---
+++ java/src/main/java/com/powsybl/python/dynamic/TimeseriesSupplier.java
@@ -0,0 +1,38 @@
+package com.powsybl.python.dynamic;
+
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.function.Supplier;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+import com.powsybl.dynamicsimulation.Curve;
+import com.powsybl.dynamicsimulation.CurvesSupplier;
+import com.powsybl.dynawaltz.DynaWaltzCurve;
+import com.powsybl.iidm.network.Network;
+
+public class TimeseriesSupplier implements CurvesSupplier {
+
+    private List<Supplier<Curve>> curvesSupplierList;
+
+    public TimeseriesSupplier() {
+        curvesSupplierList = new LinkedList<>();
+    }
+
+    public void addCurve(String dynamicId, String variable) {
+        curvesSupplierList.add(() -> new DynaWaltzCurve(dynamicId, variable));
+    }
+
+    public void addCurves(String dynamicId, Collection<String> variablesCol) {
+        for (String variable : variablesCol) {
+            addCurve(dynamicId, variable);
+        }
+    }
+
+    @Override
+    public List<Curve> get(Network network) {
+        return curvesSupplierList.stream().map(supplier -> supplier.get()).collect(Collectors.toList());
+    }
+
+}
