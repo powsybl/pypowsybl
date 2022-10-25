6abb7c647343e3e3c282d94f5b14f5a535201610
Nicolas PIERRE <nicolas.pierre@artelys.com>
2022-10-25 11:36:50 +0200

    unused import
    Signed-off-by: Nicolas PIERRE <nicolas.pierre@artelys.com>

--- java/src/main/java/com/powsybl/python/dynamic/TimeseriesSupplier.java
+++ java/src/main/java/com/powsybl/python/dynamic/TimeseriesSupplier.java
@@ -4,7 +4,6 @@ import java.util.Collection;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.function.Supplier;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 import com.powsybl.dynamicsimulation.Curve;
