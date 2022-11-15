17decbf56f9f411ebcdffb8582b27341b5e7893d
Nicolas PIERRE <nicolas.pierre@artelys.com>
2022-11-15 14:53:43 +0100

    better logging and provider interface
    Signed-off-by: Nicolas PIERRE <nicolas.pierre@artelys.com>

--- java/src/main/java/com/powsybl/python/dynamic/DynamicSimulationContext.java
+++ java/src/main/java/com/powsybl/python/dynamic/DynamicSimulationContext.java
@@ -6,24 +6,52 @@
  */
 package com.powsybl.python.dynamic;
 
+import java.util.ServiceLoader;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.powsybl.commons.PowsyblException;
 import com.powsybl.dynamicsimulation.CurvesSupplier;
 import com.powsybl.dynamicsimulation.DynamicModelsSupplier;
 import com.powsybl.dynamicsimulation.DynamicSimulation;
 import com.powsybl.dynamicsimulation.DynamicSimulationParameters;
+import com.powsybl.dynamicsimulation.DynamicSimulationProvider;
 import com.powsybl.dynamicsimulation.DynamicSimulationResult;
 import com.powsybl.dynamicsimulation.EventModelsSupplier;
 import com.powsybl.iidm.network.Network;
 
 public class DynamicSimulationContext {
+    private static Logger log = LoggerFactory.getLogger(DynamicSimulationContext.class);
+
     public DynamicSimulationResult run(Network network,
             DynamicModelsSupplier dynamicModelsSupplier,
             EventModelsSupplier eventModelsSupplier,
             CurvesSupplier curvesSupplier,
             DynamicSimulationParameters parameters) {
-        return DynamicSimulation.run(network,
+        DynamicSimulationProvider provider = getDynamicProvider("");
+        log.info("Running dynamic simulation with " + provider.getName());
+        DynamicSimulation.Runner runner = new DynamicSimulation.Runner(provider);
+        return runner.run(network,
                 dynamicModelsSupplier,
                 eventModelsSupplier,
                 curvesSupplier,
                 parameters);
+    }
+
+    /**
+     * TODO do we need to keep interface with providers here or if it should be done
+     * by the class {@link DynamicSimulation}
+     * 
+     * @param name
+     * @return
+     */
+    public static DynamicSimulationProvider getDynamicProvider(String name) {
+        String actualName = (name == null || name.isEmpty()) ? "DynaWaltz" : name;
+        return ServiceLoader.load(DynamicSimulationProvider.class).stream()
+                .map(ServiceLoader.Provider::get)
+                .filter(provider -> provider.getName().equals(actualName))
+                .findFirst()
+                .orElseThrow(() -> new PowsyblException("No dynamicSimulation provider for name '" + actualName + "'"));
     }
 }
