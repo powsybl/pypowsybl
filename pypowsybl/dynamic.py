9f40f744523ddef0e41779f5eac09f03d4c04e27
Nicolas PIERRE <nicolas.pierre@artelys.com>
2022-11-15 16:58:52 +0100

    fix python typing
    Signed-off-by: Nicolas PIERRE <nicolas.pierre@artelys.com>

--- pypowsybl/dynamic.py
+++ pypowsybl/dynamic.py
@@ -22,68 +22,68 @@ class BranchSide(_Enum):
 
 
 class ModelMapping:
-    def __init__(self):
+    def __init__(self) -> None:
         self._handle = _pp.create_dynamic_model_mapping()
 
-    def add_alphabeta_load(self, static_id: str, parameter_set_id: str):
+    def add_alphabeta_load(self, static_id: str, parameter_set_id: str) -> None:
         _pp.add_alphabeta_load(self._handle, static_id, parameter_set_id)
 
-    def add_one_transformer_load(self, static_id: str, dynamic_param: str):
+    def add_one_transformer_load(self, static_id: str, dynamic_param: str) -> None:
         _pp.add_one_transformer_load(self._handle, static_id, dynamic_param)
 
-    def add_omega_ref(self, generatorId: str):
+    def add_omega_ref(self, generatorId: str) -> None:
         _pp.add_omega_ref(self._handle, generatorId)
 
-    def add_generator_synchronous_three_windings(self, static_id: str, dynamic_param: str):
+    def add_generator_synchronous_three_windings(self, static_id: str, dynamic_param: str) -> None:
         _pp.add_generator_synchronous_three_windings(
             self._handle, static_id, dynamic_param)
 
-    def add_generator_synchronous_three_windings_proportional_regulations(self, static_id: str, dynamic_param: str):
+    def add_generator_synchronous_three_windings_proportional_regulations(self, static_id: str, dynamic_param: str) -> None:
         _pp.add_generator_synchronous_three_windings_proportional_regulations(
             self._handle, static_id, dynamic_param)
 
-    def add_generator_synchronous_four_windings(self, static_id: str, dynamic_param: str):
+    def add_generator_synchronous_four_windings(self, static_id: str, dynamic_param: str) -> None:
         _pp.add_generator_synchronous_four_windings(
             self._handle, static_id, dynamic_param)
 
-    def add_generator_synchronous_four_windings_proportional_regulations(self, static_id: str, dynamic_param: str):
+    def add_generator_synchronous_four_windings_proportional_regulations(self, static_id: str, dynamic_param: str) -> None:
         _pp.add_generator_synchronous_four_windings_proportional_regulations(
             self._handle, static_id, dynamic_param)
 
-    def add_current_limit_automaton(self, static_id: str, dynamic_param: str, branch_side: BranchSide):
+    def add_current_limit_automaton(self, static_id: str, dynamic_param: str, branch_side: BranchSide) -> None:
         _pp.add_current_limit_automaton(
             self._handle, static_id, dynamic_param, branch_side.value)
 
 
 class CurveMapping:
-    def __init__(self):
+    def __init__(self) -> None:
         self._handle = _pp.create_timeseries_mapping()
 
-    def add_curve(self, dynamic_id: str, variable: str):
+    def add_curve(self, dynamic_id: str, variable: str) -> None:
         _pp.add_curve(self._handle, dynamic_id, variable)
 
 
 class EventMapping:
-    def __init__(self):
+    def __init__(self) -> None:
         self._handle = _pp.create_event_mapping()
 
-    def add_quadripole_disconnection(self, event_model_id: str, static_id: str, parameter_set_id: str):
+    def add_quadripole_disconnection(self, event_model_id: str, static_id: str, parameter_set_id: str) -> None:
         _pp.add_event_quadripole_disconnection(
             self._handle, event_model_id, static_id, parameter_set_id)
 
-    def add_set_point_boolean(self, event_model_id: str, static_id: str, parameter_set_id: str):
+    def add_set_point_boolean(self, event_model_id: str, static_id: str, parameter_set_id: str) -> None:
         _pp.add_event_set_point_boolean(
             self._handle, event_model_id, static_id, parameter_set_id)
 
 
 class SimulationResult:
-    def __init__(self, handle: _pp.JavaHandle):
+    def __init__(self, handle: _pp.JavaHandle) -> None:
         self._handle = handle
     pass
 
 
 class Simulation:
-    def __init__(self):
+    def __init__(self) -> None:
         self._handle = _pp.create_dynamic_simulation_context()
 
     def run(self,
@@ -104,7 +104,7 @@ class Simulation:
                 start, stop)
         )
 
-    def set_config(self, path, config_name):
+    def set_config(self, path: str, config_name: str) -> None:
         os.environ["powsybl.config.dirs"] = path
         os.environ["powsybl.config.name"] = config_name
         _pp.set_powsybl_config_location(path, config_name)
