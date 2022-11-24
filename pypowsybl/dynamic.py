6ad22bc2e420b74f62d6425f1dc106b7e1212f5e
Jean-Luc Bouchot <jlbouchot@gmail.com>
2022-11-24 12:13:03 +0100

    Solves import orders
    Signed-off-by: Jean-Luc Bouchot <jlbouchot@gmail.com>

--- pypowsybl/dynamic.py
+++ pypowsybl/dynamic.py
@@ -5,13 +5,15 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 #
 
+import os
+
 from enum import Enum as _Enum
 from pypowsybl import _pypowsybl as _pp
 from pypowsybl.network import Network as _Network
 from uuid import uuid4
 from pypowsybl.util import create_data_frame_from_series_array
 from typing import List as _List
-import os
+
 import pandas as _pd
 
 class BranchSide(_Enum):
