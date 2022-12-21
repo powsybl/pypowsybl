2bef9b695e9097fb7c801c4884d7b9fe3258e656
Nicolas PIERRE <nicolas.pierre@artelys.com>
2022-12-21 16:08:26 +0100

    load_from_string clarification
    Signed-off-by: Nicolas PIERRE <nicolas.pierre@artelys.com>

--- pypowsybl/network.py
+++ pypowsybl/network.py
@@ -4513,14 +4513,15 @@ def load(file: _Union[str, _PathLike], parameters: _Dict[str, str] = None, repor
                                     None if reporter is None else reporter._reporter_model))  # pylint: disable=protected-access
 
 
-def load_from_string(file_name: str, file_content: str, parameters: _Dict[str, str] = None,
+def load_from_string(format: str, content: str, parameters: _Dict[str, str] = None,
                      reporter: _Reporter = None) -> Network:
     """
-    Load a network from a string. File content should be in a supported format.
+    Load a network from a string. format indicates which way to load the network from the content string.
+    The format must be supported.
 
     Args:
-       file_name:    file name
-       file_content: file content
+       format:       format of the data, should correspond to the file extension used by this format (XIIDM etc.)
+       content:      content of the network as a string
        parameters:   a dictionary of import parameters
 
     Returns:
@@ -4528,7 +4529,7 @@ def load_from_string(file_name: str, file_content: str, parameters: _Dict[str, s
     """
     if parameters is None:
         parameters = {}
-    return Network(_pp.load_network_from_string(file_name, file_content, parameters,
+    return Network(_pp.load_network_from_string(format, content, parameters,
                                                 None if reporter is None else reporter._reporter_model))  # pylint: disable=protected-access
 
 
