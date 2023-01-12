3c6fc861d510ba413bd8f01882140534462a1585
Etienne LESOT <etienne.lesot@rte-france.com>
2023-01-12 14:12:20 +0100

    add missing headers
    Signed-off-by: Etienne LESOT <etienne.lesot@rte-france.com>

--- java/src/main/java/com/powsybl/dataframe/network/extensions/CoordinatedReactiveControlDataframeAdder.java
+++ java/src/main/java/com/powsybl/dataframe/network/extensions/CoordinatedReactiveControlDataframeAdder.java
@@ -1,3 +1,10 @@
+/**
+ * Copyright (c) 2023, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * SPDX-License-Identifier: MPL-2.0
+ */
 package com.powsybl.dataframe.network.extensions;
 
 import com.powsybl.commons.PowsyblException;
@@ -14,6 +21,9 @@ import com.powsybl.iidm.network.extensions.CoordinatedReactiveControlAdder;
 import java.util.Collections;
 import java.util.List;
 
+/**
+ * @author Etienne Lesot <etienne.lesot@rte-france.com>
+ */
 public class CoordinatedReactiveControlDataframeAdder extends AbstractSimpleAdder {
 
     private static final List<SeriesMetadata> METADATA = List.of(
--- java/src/main/java/com/powsybl/dataframe/network/extensions/CoordinatedReactiveControlDataframeProvider.java
+++ java/src/main/java/com/powsybl/dataframe/network/extensions/CoordinatedReactiveControlDataframeProvider.java
@@ -1,3 +1,10 @@
+/**
+ * Copyright (c) 2023, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * SPDX-License-Identifier: MPL-2.0
+ */
 package com.powsybl.dataframe.network.extensions;
 
 import com.google.auto.service.AutoService;
@@ -14,6 +21,9 @@ import java.util.List;
 import java.util.Objects;
 import java.util.stream.Stream;
 
+/**
+ * @author Etienne Lesot <etienne.lesot@rte-france.com>
+ */
 @AutoService(NetworkExtensionDataframeProvider.class)
 public class CoordinatedReactiveControlDataframeProvider implements NetworkExtensionDataframeProvider {
     @Override
