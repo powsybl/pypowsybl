db52e72fc08c02e55fd9f26f53a0c1a7b327b920
Geoffroy Jamgotchian <geoffroy.jamgotchian@gmail.com>
2023-06-08 23:50:30 +0200

    Fix
    Signed-off-by: Geoffroy Jamgotchian <geoffroy.jamgotchian@gmail.com>

--- tests/test_loadflow.py
+++ tests/test_loadflow.py
@@ -236,7 +236,7 @@ def test_get_provider_parameters_names():
 
 def test_get_provider_parameters():
     specific_parameters = pp.loadflow.get_provider_parameters('OpenLoadFlow')
-    assert 45 == len(specific_parameters)
+    assert 46 == len(specific_parameters)
     assert 'Slack bus selection mode' == specific_parameters['description']['slackBusSelectionMode']
     assert 'STRING' == specific_parameters['type']['slackBusSelectionMode']
     assert 'MOST_MESHED' == specific_parameters['default']['slackBusSelectionMode']
