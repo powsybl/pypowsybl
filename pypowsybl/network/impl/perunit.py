4c8f8a7940222d3e95d3663f4e089bdd7bc2c917
Geoffroy Jamgotchian <geoffroy.jamgotchian@rte-france.com>
2023-09-21 17:25:22 +0200

    Fix per uniting of lines with not same nominal voltage at both side
    Signed-off-by: Geoffroy Jamgotchian <geoffroy.jamgotchian@rte-france.com>

--- pypowsybl/network/impl/perunit.py
+++ pypowsybl/network/impl/perunit.py
@@ -77,10 +77,28 @@ class PerUnitView:  # pylint: disable=too-many-public-methods
         for col in columns:
             df[col] /= factor
 
+    def _per_unit_r_not_same_nom_v(self, df: pd.DataFrame, columns: List[str], nominal_v1: pd.Series, nominal_v2: pd.Series) -> None:
+        factor = nominal_v1 * nominal_v2 / self.sn
+        for col in columns:
+            df[col] /= factor
+
     def _per_unit_g(self, df: pd.DataFrame, columns: List[str], nominal_v: pd.Series) -> None:
         factor = nominal_v ** 2 / self.sn
         for col in columns:
             df[col] *= factor
+
+    def y(self, df: pd.DataFrame, r_col: str, x_col: str):
+        return df.apply(lambda row: np.reciprocal(np.complex128(row[r_col] + row[x_col] * 1j)), axis=1)
+
+    def _per_unit_g_not_same_nom_v(self, df: pd.DataFrame, columns: List[str], r_col: str, x_col: str, nominal_v1: pd.Series, nominal_v2: pd.Series) -> None:
+        for col in columns:
+            ytr = self.y(df, r_col, x_col)
+            df[col] = (df[col] * nominal_v1 * nominal_v1 + (nominal_v1 - nominal_v2) * nominal_v1 * ytr.apply(lambda row: row.real)) / self.sn
+
+    def _per_unit_b_not_same_nom_v(self, df: pd.DataFrame, columns: List[str], r_col: str, x_col: str, nominal_v1: pd.Series, nominal_v2: pd.Series) -> None:
+        for col in columns:
+            ytr = self.y(df, r_col, x_col)
+            df[col] = (df[col] * nominal_v1 * nominal_v1 + (nominal_v1 - nominal_v2) * nominal_v1 * ytr.apply(lambda row: row.imag)) / self.sn
 
     def _per_unit_i(self, df: pd.DataFrame, columns: List[str], nominal_v: pd.Series) -> None:
         factor = self.sn * 10 ** 3 / (self.sqrt3 * nominal_v)
@@ -163,11 +181,16 @@ class PerUnitView:  # pylint: disable=too-many-public-methods
             a per-united dataframe of lines.
         """
         lines = self._network.get_lines()
-        nominal_v = self._get_indexed_nominal_v(lines, 'voltage_level2_id')
+        nominal_v1 = self._get_indexed_nominal_v(lines, 'voltage_level1_id')
+        nominal_v2 = self._get_indexed_nominal_v(lines, 'voltage_level2_id')
         self._per_unit_p(lines, ['p1', 'p2', 'q1', 'q2'])
-        self._per_unit_i(lines, ['i1', 'i2'], nominal_v)
-        self._per_unit_r(lines, ['r', 'x'], nominal_v)
-        self._per_unit_g(lines, ['g1', 'g2', 'b1', 'b2'], nominal_v)
+        self._per_unit_i(lines, ['i1'], nominal_v1)
+        self._per_unit_i(lines, ['i2'], nominal_v2)
+        self._per_unit_g_not_same_nom_v(lines, ['g1'], 'r', 'x', nominal_v1, nominal_v2)
+        self._per_unit_g_not_same_nom_v(lines, ['g2'], 'r', 'x', nominal_v2, nominal_v1)
+        self._per_unit_b_not_same_nom_v(lines, ['b1'], 'r', 'x', nominal_v1, nominal_v2)
+        self._per_unit_b_not_same_nom_v(lines, ['b2'], 'r', 'x', nominal_v2, nominal_v1)
+        self._per_unit_r_not_same_nom_v(lines, ['r', 'x'], nominal_v1, nominal_v2)
         return lines
 
     def get_2_windings_transformers(self) -> pd.DataFrame:
