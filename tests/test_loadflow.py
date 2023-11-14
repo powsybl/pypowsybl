aaeccb09745e019ff727563da39abcef515de355
Geoffroy Jamgotchian <geoffroy.jamgotchian@rte-france.com>
2023-11-14 08:32:23 +0100

    Upgrade to powsybl dependencies 2023.3.2 (#673)
    Signed-off-by: Geoffroy Jamgotchian <geoffroy.jamgotchian@gmail.com>

--- tests/test_loadflow.py
+++ tests/test_loadflow.py
@@ -189,7 +189,7 @@ def test_get_provider_parameters_names():
         'slackBusesIds',
         'lowImpedanceBranchMode',
         'voltageRemoteControl',
-        'throwsExceptionInCaseOfSlackDistributionFailure',
+        'slackDistributionFailureBehavior',
         'loadPowerFactorConstant',
         'plausibleActivePowerLimit',
         'slackBusPMaxMismatch',
@@ -231,13 +231,22 @@ def test_get_provider_parameters_names():
         'asymmetrical',
         'minNominalVoltageTargetVoltageCheck',
         'reactivePowerDispatchMode',
-        'outerLoopNames'
+        'outerLoopNames',
+        'useActiveLimits',
+        'lineSearchStateVectorScalingMaxIteration',
+        'lineSearchStateVectorScalingStepFold',
+        'maxVoltageChangeStateVectorScalingMaxDv',
+        'maxVoltageChangeStateVectorScalingMaxDphi',
+        'linePerUnitMode',
+        'useLoadModel',
+        'dcApproximationType',
+        'simulateAutomationSystems'
     ]
 
 
 def test_get_provider_parameters():
     specific_parameters = pp.loadflow.get_provider_parameters('OpenLoadFlow')
-    assert 47 == len(specific_parameters)
+    assert 56 == len(specific_parameters)
     assert 'Slack bus selection mode' == specific_parameters['description']['slackBusSelectionMode']
     assert 'STRING' == specific_parameters['type']['slackBusSelectionMode']
     assert 'MOST_MESHED' == specific_parameters['default']['slackBusSelectionMode']
