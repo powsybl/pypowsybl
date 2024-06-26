d1e47e7f2acbc9eab32164b39e866e2b01f82123
Etienne LESOT <etienne.lesot@rte-france.com>
2024-06-26 09:15:22 +0200

    try to fix ci
    Signed-off-by: Etienne LESOT <etienne.lesot@rte-france.com>

--- tests/test_loadflow.py
+++ tests/test_loadflow.py
@@ -336,3 +336,6 @@ def test_wrong_regulated_bus_id():
     pp.loadflow.run_ac(net)
     parameters = lf.ValidationParameters()
     validation = pp.loadflow.run_validation(net, validation_parameters=parameters)
+
+def test():
+    pass
