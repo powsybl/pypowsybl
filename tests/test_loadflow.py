9f2c14e7e4e4a631fd8d57903d955767f5a3c456
Geoffroy Jamgotchian <geoffroy.jamgotchian@rte-france.com>
2024-06-26 10:29:56 +0200

    Upgrade to PowSyBl 2024.1.2
    Signed-off-by: Geoffroy Jamgotchian <geoffroy.jamgotchian@rte-france.com>

--- tests/test_loadflow.py
+++ tests/test_loadflow.py
@@ -270,11 +270,12 @@ def test_get_provider_parameters_names():
                                    'newtonKrylovLineSearch',
                                    'referenceBusSelectionMode',
                                    'writeReferenceTerminals',
-                                   'voltageTargetPriorities']
+                                   'voltageTargetPriorities',
+                                   'generatorVoltageControlMinNominalVoltage']
 
 def test_get_provider_parameters():
     specific_parameters = pp.loadflow.get_provider_parameters('OpenLoadFlow')
-    assert 65 == len(specific_parameters)
+    assert 66 == len(specific_parameters)
     assert 'Slack bus selection mode' == specific_parameters['description']['slackBusSelectionMode']
     assert 'STRING' == specific_parameters['type']['slackBusSelectionMode']
     assert 'MOST_MESHED' == specific_parameters['default']['slackBusSelectionMode']
