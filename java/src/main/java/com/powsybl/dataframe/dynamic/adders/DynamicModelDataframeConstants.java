adbae2d57a6a269e07533c816cee8a5b834fadb0
lisrte <laurent.issertial@rte-france.com>
2024-07-24 15:39:26 +0200

    Refactor SynchronousGeneratorAdder Add adder tests
    Signed-off-by: lisrte <laurent.issertial@rte-france.com>

---
+++ java/src/main/java/com/powsybl/dataframe/dynamic/adders/DynamicModelDataframeConstants.java
@@ -0,0 +1,20 @@
+/**
+ * Copyright (c) 2024, RTE (http://www.rte-france.com/)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+package com.powsybl.dataframe.dynamic.adders;
+
+/**
+ * @author Laurent Issertial {@literal <laurent.issertial at rte-france.com>}
+ */
+public class DynamicModelDataframeConstants {
+
+    public static final String STATIC_ID = "static_id";
+    public static final String PARAMETER_SET_ID = "parameter_set_id";
+    public static final String MODEL_NAME = "model_name";
+
+    private DynamicModelDataframeConstants() {}
+}
