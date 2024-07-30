5e14beeb154b5e7a52e8b825d3d5e04fc656a096
Coline Piloquet <55250145+colinepiloquet@users.noreply.github.com>
2024-07-30 14:30:35 +0200

    Make pairing key of dangling lines updatable (#811)
    Signed-off-by: Coline PILOQUET <coline.piloquet@rte-france.com>

--- java/src/main/java/com/powsybl/dataframe/network/NetworkDataframes.java
+++ java/src/main/java/com/powsybl/dataframe/network/NetworkDataframes.java
@@ -603,7 +603,7 @@ public final class NetworkDataframes {
                 .strings("bus_breaker_bus_id", getBusBreakerViewBusId(), NetworkDataframes::setBusBreakerViewBusId, false)
                 .ints("node", dl -> getNode(dl.getTerminal()), false)
                 .booleans("connected", dl -> dl.getTerminal().isConnected(), connectInjection())
-                .strings("pairing_key", dl -> Objects.toString(dl.getPairingKey(), ""))
+                .strings("pairing_key", dl -> Objects.toString(dl.getPairingKey(), ""), DanglingLine::setPairingKey)
                 .strings("ucte_xnode_code", dl -> Objects.toString(dl.getPairingKey(), ""))
                 .booleans("fictitious", Identifiable::isFictitious, Identifiable::setFictitious, false)
                 .strings("tie_line_id", dl -> dl.getTieLine().map(Identifiable::getId).orElse(""))
