d198dc7c49e66aba23a486a172c92df477309b59
Choco <97348238+unetablettedechocolat@users.noreply.github.com>
2024-07-31 10:29:33 +0200

    Display units feeder info (#799)
    Signed-off-by: Tiphaine Mouminous <tiphaine.mouminous_externe@rte-france.com>

--- cpp/pypowsybl-cpp/bindings.cpp
+++ cpp/pypowsybl-cpp/bindings.cpp
@@ -548,7 +548,10 @@ PYBIND11_MODULE(_pypowsybl, m) {
         .def_readwrite("tooltip_enabled", &pypowsybl::SldParameters::tooltip_enabled)
         .def_readwrite("topological_coloring", &pypowsybl::SldParameters::topological_coloring)
         .def_readwrite("component_library", &pypowsybl::SldParameters::component_library)
-        .def_readwrite("display_current_feeder_info", &pypowsybl::SldParameters::display_current_feeder_info);
+        .def_readwrite("display_current_feeder_info", &pypowsybl::SldParameters::display_current_feeder_info)
+        .def_readwrite("active_power_unit", &pypowsybl::SldParameters::active_power_unit)
+        .def_readwrite("reactive_power_unit", &pypowsybl::SldParameters::reactive_power_unit)
+        .def_readwrite("current_unit", &pypowsybl::SldParameters::current_unit);
 
     py::enum_<pypowsybl::NadLayoutType>(m, "NadLayoutType")
             .value("FORCE_LAYOUT", pypowsybl::NadLayoutType::FORCE_LAYOUT)
