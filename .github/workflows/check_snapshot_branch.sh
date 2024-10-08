2697b6ab48705b648b852071d55d5d834b91ab8f
Leclerc Clement <clement.leclerc@rte-france.com>
2024-10-08 10:02:29 +0200

    snapshot ci
    Signed-off-by: Leclerc Clement <clement.leclerc@rte-france.com>

--- .github/workflows/check_snapshot_branch.sh
+++ .github/workflows/check_snapshot_branch.sh
@@ -5,7 +5,6 @@ check_snapshot_branch() {
     local core_version=$2
 
     SNAPSHOT_BRANCH=$(git ls-remote --heads "$repo" | grep -E "refs/heads/$(echo $core_version | grep -q SNAPSHOT && echo "$core_version" || echo "$core_version-SNAPSHOT")" | sed 's/.*refs\/heads\///')
-
     if [ -n "$SNAPSHOT_BRANCH" ]; then
         echo "SNAPSHOT VERSION EXIST: $SNAPSHOT_BRANCH"
         echo "SNAPSHOT_BRANCH=$SNAPSHOT_BRANCH" >> $GITHUB_ENV
