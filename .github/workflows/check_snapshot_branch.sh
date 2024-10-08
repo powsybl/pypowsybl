d34e1caf26bd466a0c6060915245878b43ae730a
Leclerc Clement <clement.leclerc@rte-france.com>
2024-10-08 11:04:50 +0200

    snapshot ci
    Signed-off-by: Leclerc Clement <clement.leclerc@rte-france.com>

--- .github/workflows/check_snapshot_branch.sh
+++ .github/workflows/check_snapshot_branch.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-check_snapshot_branch() {
-    local repo=$1
-    local core_version=$2
 
-    SNAPSHOT_BRANCH=$(git ls-remote --heads "$repo" | grep -E "refs/heads/$(echo $core_version | grep -q SNAPSHOT && echo "$core_version" || echo "$core_version-SNAPSHOT")" | sed 's/.*refs\/heads\///')
-    if [ -n "$SNAPSHOT_BRANCH" ]; then
-        echo "SNAPSHOT VERSION EXIST: $SNAPSHOT_BRANCH"
-        echo "SNAPSHOT_BRANCH=$SNAPSHOT_BRANCH" >> $GITHUB_ENV
-    else
-        echo "No SNAPSHOT branch found"
-        echo "SNAPSHOT_BRANCH=main" >> $GITHUB_ENV
-    fi
-}
+repo=$1
+core_version=$2
+
+echo "$1"
+
+SNAPSHOT_BRANCH=$(git ls-remote --heads "$repo" | grep -E "refs/heads/$(echo $core_version | grep -q SNAPSHOT && echo "$core_version" || echo "$core_version-SNAPSHOT")" | sed 's/.*refs\/heads\///')
+if [ -n "$SNAPSHOT_BRANCH" ]; then
+    echo "SNAPSHOT VERSION EXIST: $SNAPSHOT_BRANCH"
+    echo "SNAPSHOT_BRANCH=$SNAPSHOT_BRANCH" >> $GITHUB_ENV
+else
+    echo "No SNAPSHOT branch found"
+    echo "SNAPSHOT_BRANCH=main" >> $GITHUB_ENV
+fi
