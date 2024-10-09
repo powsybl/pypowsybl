1c5d9b6b6bc70a95458818eb234525df21445483
Naledi EL CHEIKH <naledi.elcheikh@rte-france.com>
2024-10-09 11:11:44 +0200

    update : environment OK
    Signed-off-by: Naledi EL CHEIKH <naledi.elcheikh@rte-france.com>

--- tests/test_network_extensions.py
+++ tests/test_network_extensions.py
@@ -467,18 +467,6 @@ def test_cgmes_metadata_extension():
     n.create_extensions(extension_name, [metadata])
     test = n.get_extensions(extension_name)
 
-    metadata_2 = pd.DataFrame.from_records(index='id',
-                                           columns=['id', 'cgmes_subset', 'description', 'version',
-                                                    'modeling_authority_set', 'profiles', 'dependent_on', 'supersedes'],
-                                           data=[('sshId', 'EQUIPMENT', 'SSH description', 2,
-                                                  'http://powsybl.org', 'equipment',
-                                                  'ssh-dependency1/ssh-dependency2', '')])
-
-    n.update_extensions(extension_name, metadata_2)
-    test2 = n.get_extensions(extension_name)
-
-    assert metadata_2.equals(test2)
-    assert test2.version == 2
 
 
 
