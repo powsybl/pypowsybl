1d7e42418ce87d38bdc95ff4fe6f096e96cd9621
Geoffroy Jamgotchian <geoffroy.jamgotchian@rte-france.com>
2024-10-10 12:56:41 +0200

    Fix Sonar issues
    Signed-off-by: Geoffroy Jamgotchian <geoffroy.jamgotchian@rte-france.com>

--- pypowsybl/network/impl/pandapower_converter.py
+++ pypowsybl/network/impl/pandapower_converter.py
@@ -35,20 +35,25 @@ def convert_from_pandapower(n_pdp: pandapowerNet) -> Network:
         create_shunts(n, n_pdp)
         create_lines(n, n_pdp)
         create_transformers(n, n_pdp)
-
-        if len(slack_weight_by_gen_id) > 0:
-            highest_weight_gen_id = max(slack_weight_by_gen_id, key=lambda k: slack_weight_by_gen_id[k])
-            for index, (gen_id, weight) in enumerate(slack_weight_by_gen_id.items()):
-                if gen_id == highest_weight_gen_id:
-                    # create slack bus extension for first one
-                    generators = n.get_generators(attributes=['voltage_level_id'])
-                    slack_gen = generators.loc[gen_id]
-                    n.create_extensions(extension_name='slackTerminal', element_id=gen_id, voltage_level_id=slack_gen['voltage_level_id'])
-
-                # create active power control extension to define the distribution key
-                n.create_extensions(extension_name='activePowerControl', id=gen_id, participate=True if weight != 0.0 else False, droop=weight)
+        create_extensions(n, slack_weight_by_gen_id)
 
         return n
+
+
+def create_extensions(n: Network, slack_weight_by_gen_id: Dict[str, float]) -> None:
+    if len(slack_weight_by_gen_id) > 0:
+        highest_weight_gen_id = max(slack_weight_by_gen_id, key=lambda k: slack_weight_by_gen_id[k])
+        for index, (gen_id, weight) in enumerate(slack_weight_by_gen_id.items()):
+            if gen_id == highest_weight_gen_id:
+                # create slack bus extension for first one
+                generators = n.get_generators(attributes=['voltage_level_id'])
+                slack_gen = generators.loc[gen_id]
+                n.create_extensions(extension_name='slackTerminal', element_id=gen_id,
+                                    voltage_level_id=slack_gen['voltage_level_id'])
+
+            # create active power control extension to define the distribution key
+            n.create_extensions(extension_name='activePowerControl', id=gen_id,
+                                participate=True if abs(weight) > 0.001 else False, droop=weight)
 
 
 def get_name(df: pd.DataFrame, name: str) -> pd.Series:
@@ -98,7 +103,7 @@ def create_transformers(n: Network, n_pdp: pandapowerNet) -> None:
     if len(n_pdp.trafo) > 0:
         bus = n_pdp.bus[['vn_kv']]
         trafo_and_bus = n_pdp.trafo.merge(bus.rename(columns=lambda x: x + '_lv_bus'), left_on='lv_bus', right_index=True, how='inner')
-        id = generate_transformer_id(trafo_and_bus, len(n_pdp.line))
+        trafo_id = generate_transformer_id(trafo_and_bus, len(n_pdp.line))
         name = get_name(trafo_and_bus, 'name')
         vl1_id = build_voltage_level_id(trafo_and_bus['hv_bus'].astype(str))
         vl2_id = build_voltage_level_id(trafo_and_bus['lv_bus'].astype(str))
@@ -124,7 +129,7 @@ def create_transformers(n: Network, n_pdp: pandapowerNet) -> None:
         g = gm / zb_tr * trafo_and_bus['parallel']
         b = bm / zb_tr * trafo_and_bus['parallel']
 
-        n.create_2_windings_transformers(id=id, name=name,
+        n.create_2_windings_transformers(id=trafo_id, name=name,
                                          voltage_level1_id=vl1_id, connectable_bus1_id=connectable_bus1_id, bus1_id=bus1_id,
                                          voltage_level2_id=vl2_id, connectable_bus2_id=connectable_bus2_id, bus2_id=bus2_id,
                                          rated_u1=rated_u1, rated_u2=rated_u2,
@@ -143,7 +148,7 @@ def create_limits(n: Network, n_pdp: pandapowerNet, id: pd.Series) -> None:
 
 def create_lines(n: Network, n_pdp: pandapowerNet) -> None:
     if len(n_pdp.line) > 0:
-        id = generate_line_id(n_pdp.line)
+        line_id = generate_line_id(n_pdp.line)
         name = get_name(n_pdp.line, 'name')
         vl1_id = build_voltage_level_id(n_pdp.line['from_bus'].astype(str))
         vl2_id = build_voltage_level_id(n_pdp.line['to_bus'].astype(str))
@@ -156,16 +161,16 @@ def create_lines(n: Network, n_pdp: pandapowerNet) -> None:
         g = n_pdp.line['length_km'] * n_pdp.line['g_us_per_km'] * 1e-6 * n_pdp.line['parallel'] / 2
         b = n_pdp.line['length_km'] * n_pdp.line['c_nf_per_km'] * 1e-9 * 2 * math.pi * n_pdp.f_hz * n_pdp.line['parallel'] / 2
 
-        n.create_lines(id=id, name=name,
+        n.create_lines(id=line_id, name=name,
                        voltage_level1_id=vl1_id, connectable_bus1_id=connectable_bus1_id, bus1_id=bus1_id,
                        voltage_level2_id=vl2_id, connectable_bus2_id=connectable_bus2_id, bus2_id=bus2_id,
                        r=r, x=x, g1=g, g2=g, b1=b, b2=b)
-        create_limits(n, n_pdp, id)
+        create_limits(n, n_pdp, line_id)
 
 
 def create_shunts(n: Network, n_pdp: pandapowerNet) -> None:
     if len(n_pdp.shunt) > 0:
-        id = generate_injection_id(n_pdp.shunt, 'shunt')
+        shunt_id = generate_injection_id(n_pdp.shunt, 'shunt')
         name = get_name(n_pdp.shunt, 'name').tolist()
         vl_id = build_voltage_level_id(n_pdp.shunt['bus'].astype(str)).tolist()
         connectable_bus_id = build_bus_id(n_pdp.shunt['bus'].astype(str)).tolist()
@@ -179,7 +184,7 @@ def create_shunts(n: Network, n_pdp: pandapowerNet) -> None:
             'bus_id': bus_id,
             'model_type': model_type,
             'section_count': section_count
-        }, index=id)
+        }, index=shunt_id)
         g_per_section = (n_pdp.shunt['p_mw'] / (n_pdp.shunt['vn_kv'] ** 2) * -1.0).tolist()
         b_per_section = (n_pdp.shunt['q_mvar'] / (n_pdp.shunt['vn_kv'] ** 2) * -1.0).tolist()
         max_section_count = n_pdp.shunt['max_step'].tolist()
@@ -187,7 +192,7 @@ def create_shunts(n: Network, n_pdp: pandapowerNet) -> None:
             'g_per_section': g_per_section,
             'b_per_section': b_per_section,
             'max_section_count': max_section_count,
-        }, index=id)
+        }, index=shunt_id)
         n.create_shunt_compensators(shunt_df=shunt_df, linear_model_df=linear_model_df)
 
 class PandaPowerGeneratorType(Enum):
@@ -198,32 +203,64 @@ class PandaPowerGeneratorType(Enum):
 def _create_generators(n: Network, gen: DataFrame, bus: DataFrame, slack_weight_by_gen_id: Dict[str, float], generator_type: PandaPowerGeneratorType) -> None:
     if len(gen) > 0:
         gen_and_bus: DataFrame = gen.merge(bus, left_on='bus', right_index=True, how='inner', suffixes=('', '_x'))
-        id = generate_injection_id(gen_and_bus, 'gen' if generator_type != PandaPowerGeneratorType.STATIC_GENERATOR else 'sgen')
+        gen_id = generate_injection_id(gen_and_bus, 'gen' if generator_type != PandaPowerGeneratorType.STATIC_GENERATOR else 'sgen')
         name = get_name(gen_and_bus, 'name')
         vl_id = build_voltage_level_id(gen_and_bus['bus'].astype(str))
         connectable_bus_id = build_bus_id(gen_and_bus['bus'].astype(str)).tolist()
         bus_id = np.where(gen_and_bus['in_service'], connectable_bus_id, "")
-        target_p = pd.Series([MIN_TARGET_P_TO_NOT_BEEN_DISCADED_FROM_ACTIVE_POWER_CONTROL] * len(gen_and_bus)) if generator_type == PandaPowerGeneratorType.EXT_GRID else gen_and_bus['p_mw'] # keep a small value of target_p to avoid be discarded to slack distribution
+        target_p = create_generator_target_p(gen_and_bus, generator_type)
         voltage_regulator_on = [True] * len(gen_and_bus)
-        target_v = gen_and_bus['vm_pu'] * gen_and_bus['vn_kv'] if generator_type != PandaPowerGeneratorType.STATIC_GENERATOR else pd.Series([1.0] * len(gen_and_bus))
-        target_q = gen_and_bus['q_mvar'] if generator_type == PandaPowerGeneratorType.STATIC_GENERATOR in gen_and_bus.columns else pd.Series([0.0] * len(gen_and_bus))
-        min_p = pd.Series([DEFAULT_MIN_P] * len(gen_and_bus)) if generator_type == PandaPowerGeneratorType.EXT_GRID or 'min_p_mw' not in gen_and_bus.columns else pd.Series(np.nan_to_num(gen_and_bus['min_p_mw'], nan=DEFAULT_MIN_P))
-        max_p = pd.Series([DEFAULT_MAX_P] * len(gen_and_bus)) if generator_type == PandaPowerGeneratorType.EXT_GRID or 'max_p_mw' not in gen_and_bus.columns else pd.Series(np.nan_to_num(gen_and_bus['max_p_mw'], nan=DEFAULT_MAX_P))
+        target_v = create_generator_target_v(gen_and_bus, generator_type)
+        target_q = create_generator_target_q(gen_and_bus, generator_type)
+        min_p = create_generator_min_p(gen_and_bus, generator_type)
+        max_p = create_generator_max_p(gen_and_bus, generator_type)
         min_q = gen_and_bus['min_q_mvar'] if 'min_q_mvar' in gen_and_bus.columns else None
         max_q = gen_and_bus['max_q_mvar'] if 'max_q_mvar' in gen_and_bus.columns else None
 
-        if generator_type != PandaPowerGeneratorType.STATIC_GENERATOR:
-            for index, row in gen_and_bus.iterrows():
-                index = typing.cast(int, index) # safe cas from hashtable to int needed by mypy
-                weight = row['slack_weight'] if generator_type == PandaPowerGeneratorType.EXT_GRID or row['slack'] else 0.0
-                slack_weight_by_gen_id[build_injection_id('gen', row['bus'], index)] = weight
+        fill_generator_slack_weight(gen_and_bus, generator_type, slack_weight_by_gen_id)
 
-        n.create_generators(id=id, name=name,
+        n.create_generators(id=gen_id, name=name,
                             voltage_level_id=vl_id, connectable_bus_id=connectable_bus_id, bus_id=bus_id,
                             target_p=target_p,  voltage_regulator_on=voltage_regulator_on, target_v=target_v, target_q=target_q,
                             min_p=min_p, max_p=max_p)
         if min_q is not None and max_q is not None:
-            n.create_minmax_reactive_limits(id=id, min_q=min_q, max_q=max_q)
+            n.create_minmax_reactive_limits(id=gen_id, min_q=min_q, max_q=max_q)
+
+
+def create_generator_target_p(gen_and_bus: DataFrame, generator_type: PandaPowerGeneratorType) -> pd.Series:
+    return pd.Series([MIN_TARGET_P_TO_NOT_BEEN_DISCADED_FROM_ACTIVE_POWER_CONTROL] * len(
+        gen_and_bus)) if generator_type == PandaPowerGeneratorType.EXT_GRID else gen_and_bus['p_mw'] # keep a small value of target_p to avoid be discarded to slack distribution
+
+
+def create_generator_max_p(gen_and_bus: DataFrame, generator_type: PandaPowerGeneratorType) -> pd.Series:
+    return pd.Series([DEFAULT_MAX_P] * len(
+        gen_and_bus)) if generator_type == PandaPowerGeneratorType.EXT_GRID or 'max_p_mw' not in gen_and_bus.columns else pd.Series(
+        np.nan_to_num(gen_and_bus['max_p_mw'], nan=DEFAULT_MAX_P))
+
+
+def create_generator_min_p(gen_and_bus: DataFrame, generator_type: PandaPowerGeneratorType) -> pd.Series:
+    return pd.Series([DEFAULT_MIN_P] * len(
+        gen_and_bus)) if generator_type == PandaPowerGeneratorType.EXT_GRID or 'min_p_mw' not in gen_and_bus.columns else pd.Series(
+        np.nan_to_num(gen_and_bus['min_p_mw'], nan=DEFAULT_MIN_P))
+
+
+def create_generator_target_q(gen_and_bus: DataFrame, generator_type: PandaPowerGeneratorType) -> pd.Series:
+    return gen_and_bus[
+        'q_mvar'] if generator_type == PandaPowerGeneratorType.STATIC_GENERATOR in gen_and_bus.columns else pd.Series(
+        [0.0] * len(gen_and_bus))
+
+
+def create_generator_target_v(gen_and_bus: DataFrame, generator_type: PandaPowerGeneratorType) -> pd.Series:
+    return gen_and_bus['vm_pu'] * gen_and_bus[
+        'vn_kv'] if generator_type != PandaPowerGeneratorType.STATIC_GENERATOR else pd.Series([1.0] * len(gen_and_bus))
+
+
+def fill_generator_slack_weight(gen_and_bus: DataFrame, generator_type: PandaPowerGeneratorType, slack_weight_by_gen_id: Dict[str, float]) -> None:
+    if generator_type != PandaPowerGeneratorType.STATIC_GENERATOR:
+        for index, row in gen_and_bus.iterrows():
+            index = typing.cast(int, index)  # safe cas from hashtable to int needed by mypy
+            weight = row['slack_weight'] if generator_type == PandaPowerGeneratorType.EXT_GRID or row['slack'] else 0.0
+            slack_weight_by_gen_id[build_injection_id('gen', row['bus'], index)] = weight
 
 
 def create_generators(n: Network, n_pdp: pandapowerNet, slack_weight_by_gen_id: Dict[str, float]) -> None:
@@ -234,14 +271,14 @@ def create_generators(n: Network, n_pdp: pandapowerNet, slack_weight_by_gen_id: 
 
 def create_loads(n: Network, n_pdp: pandapowerNet) -> None:
     if len(n_pdp.load) > 0:
-        id = generate_injection_id(n_pdp.load, 'load')
+        load_id = generate_injection_id(n_pdp.load, 'load')
         name = get_name(n_pdp.load, 'name')
         vl_id = build_voltage_level_id(n_pdp.load['bus'].astype(str))
         connectable_bus_id = build_bus_id(n_pdp.load['bus'].astype(str)).tolist()
         bus_id = np.where(n_pdp.load['in_service'], connectable_bus_id, "")
         p0 = n_pdp.load['p_mw']
         q0 = n_pdp.load['q_mvar']
-        n.create_loads(id=id, name=name,
+        n.create_loads(id=load_id, name=name,
                        voltage_level_id=vl_id, connectable_bus_id=connectable_bus_id, bus_id=bus_id,
                        p0=p0, q0=q0)
 
@@ -254,9 +291,9 @@ def create_buses(n: Network, n_pdp: pandapowerNet) -> None:
         low_voltage_limit = n_pdp.bus['min_vm_pu'] * nominal_v if 'min_vm_pu' in n_pdp.bus.columns else None
         high_voltage_limit = n_pdp.bus['max_vm_pu'] * nominal_v if 'max_vm_pu' in n_pdp.bus.columns else None
         substation_id = ['s'] * len(n_pdp.bus)
-        # FIXME topology kind should have a default value
+        # TODO topology kind should have a default value
         n.create_voltage_levels(id=vl_id, substation_id=substation_id, topology_kind=topology_kind, nominal_v=nominal_v,
                                 low_voltage_limit=low_voltage_limit, high_voltage_limit=high_voltage_limit)
-        id = build_bus_id(n_pdp.bus.index.astype(str))
+        bus_id = build_bus_id(n_pdp.bus.index.astype(str))
         name = get_name(n_pdp.bus, 'name')
-        n.create_buses(id=id, name=name, voltage_level_id=vl_id)
+        n.create_buses(id=bus_id, name=name, voltage_level_id=vl_id)
