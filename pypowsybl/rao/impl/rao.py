c99e12fd4ca5296e36f6e7cb1ebf0777e6a04b26
Bertrand Rix <bertrand.rix@artelys.com>
2024-10-21 11:29:57 +0200

    Memory view instead of BytesIO directly.
    Signed-off-by: Bertrand Rix <bertrand.rix@artelys.com>

--- pypowsybl/rao/impl/rao.py
+++ pypowsybl/rao/impl/rao.py
@@ -15,7 +15,7 @@ class Rao:
         _pypowsybl.run_rao(network._handle, self._handle, crac_file, parameter_file, glsk_file)
 
     def run_with_buffers(self, network: Network, crac_buffer: io.BytesIO, parameter_buffer: io.BytesIO, glsk_buffer: io.BytesIO) -> None:
-        _pypowsybl.run_rao_from_buffers(network._handle, self._handle, crac_buffer, parameter_buffer, glsk_buffer)
+        _pypowsybl.run_rao_from_buffers(network._handle, self._handle, crac_buffer.getbuffer(), parameter_buffer.getbuffer(), glsk_buffer.getbuffer())
 
     def serialize_rao_results(self, output_file: str) -> None:
         _pypowsybl.serialize_rao_results_to_file(self._handle, output_file)
