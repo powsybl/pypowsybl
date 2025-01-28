47b4ffc6f6963d4d3e12514ec31857672868be05
Nicolas Rol <nicolas.rol@rte-france.com>
2025-01-28 17:27:09 +0100

    print
    Signed-off-by: Nicolas Rol <nicolas.rol@rte-france.com>

--- .github/workflows/scripts/build_module.sh
+++ .github/workflows/scripts/build_module.sh
@@ -5,11 +5,12 @@ COMMAND=$2
 CURRENT_OS=$3
 CURRENT_PYTHON=$4
 RESULT_FILE="job_result_$CURRENT_OS-$CURRENT_PYTHON.txt"
+echo "$RESULT_FILE"
+cat "$RESULT_FILE"
 
 echo "Building $MODULE_NAME..."
 $COMMAND
 BUILD_EXIT=$?
-echo "$RESULT_FILE"
 if [ $BUILD_EXIT -ne 0 ]; then
     printf '%s;%s;%s;failure\n' "$CURRENT_OS" "$CURRENT_PYTHON" "$MODULE_NAME" >> "$RESULT_FILE"
 else
