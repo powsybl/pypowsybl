642fb9aae763633b584cbfb1170bcc4d064d8482
Nicolas Rol <nicolas.rol@rte-france.com>
2025-01-28 14:18:39 +0100

    rework CI snapshot
    Signed-off-by: Nicolas Rol <nicolas.rol@rte-france.com>

--- .github/workflows/scripts/build_module.sh
+++ .github/workflows/scripts/build_module.sh
@@ -2,15 +2,18 @@
 
 MODULE_NAME=$1
 COMMAND=$2
+CURRENT_OS=$3
+CURRENT_PYTHON=$4
+RESULT_FILE="job_result_$CURRENT_OS-$CURRENT_PYTHON.txt"
 
 echo "Building $MODULE_NAME..."
 $COMMAND
 BUILD_EXIT=$?
 
 if [ $BUILD_EXIT -ne 0 ]; then
-    echo "❌ $MODULE_NAME build FAILED" >> $BUILD_STATUS
+    printf '%s;%s;%s;failure\n' "$CURRENT_OS" "$CURRENT_PYTHON" "$MODULE_NAME" >> "$RESULT_FILE"
 else
-    echo "✅ $MODULE_NAME build SUCCESS" >> $BUILD_STATUS
+    printf '%s;%s;%s;success\n' "$CURRENT_OS" "$CURRENT_PYTHON" "$MODULE_NAME" >> "$RESULT_FILE"
 fi
 
 exit $BUILD_EXIT
