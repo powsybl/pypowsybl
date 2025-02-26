0d8ecd55c333bbdacaf3e6a3e7d6f21ed204008e
Nicolas Rol <nicolas.rol@rte-france.com>
2025-02-26 09:44:49 +0100

    fix
    Signed-off-by: Nicolas Rol <nicolas.rol@rte-france.com>

--- .github/workflows/scripts/check_snapshot_branch.sh
+++ .github/workflows/scripts/check_snapshot_branch.sh
@@ -7,11 +7,11 @@ core_version=$2
 core_snapshot_version=$(echo "$core_version" | grep -q SNAPSHOT && echo "$core_version" || echo "$core_version-SNAPSHOT")
 
 # Find if an CI snapshot branch exists
-CI_SNAPSHOT_BRANCH=$(git ls-remote --heads "$repo" | grep -E "refs/heads/ci/core-$core_snapshot_version" | sed 's/.*refs\/heads\///')
-if [ -n "$CI_SNAPSHOT_BRANCH" ]; then
-    echo "SNAPSHOT VERSION EXIST: $CI_SNAPSHOT_BRANCH"
-    echo "CI_SNAPSHOT_BRANCH=$CI_SNAPSHOT_BRANCH" >> "$GITHUB_ENV"
+SNAPSHOT_BRANCH=$(git ls-remote --heads "$repo" | grep -E "refs/heads/ci/core-$core_snapshot_version" | sed 's/.*refs\/heads\///')
+if [ -n "$SNAPSHOT_BRANCH" ]; then
+    echo "SNAPSHOT VERSION EXIST: $SNAPSHOT_BRANCH"
+    echo "SNAPSHOT_BRANCH=$SNAPSHOT_BRANCH" >> "$GITHUB_ENV"
 else
     echo "No SNAPSHOT branch found"
-    echo "CI_SNAPSHOT_BRANCH=main" >> "$GITHUB_ENV"
+    echo "SNAPSHOT_BRANCH=main" >> "$GITHUB_ENV"
 fi
