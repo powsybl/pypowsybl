0d05b0706467e17cb240ce9bda515c236e6bf9d5
Geoffroy Jamgotchian <geoffroy.jamgotchian@gmail.com>
2025-04-18 00:24:32 +0200

    Wip
    Signed-off-by: Geoffroy Jamgotchian <geoffroy.jamgotchian@gmail.com>

--- pypowsybl/opf/impl/opf.py
+++ pypowsybl/opf/impl/opf.py
@@ -1,10 +1,16 @@
+from dataclasses import dataclass
 from math import hypot, atan2
+from typing import Any
+import logging
 
 import pyoptinterface as poi
 from pyoptinterface import nlfunc, ipopt
 
 from pypowsybl import PyPowsyblError
 from pypowsybl.network import Network
+
+logger = logging.getLogger(__name__)
+
 
 R2 = 1.0
 A2 = 0.0
@@ -65,6 +71,64 @@ def shunt_flow(vars, params):
 
     return [p_eq, q_eq]
 
+class NetworkCache:
+    def __init__(self, network: Network) -> None:
+        self._network = network
+        self._network.per_unit = True
+        self._buses = self._network.get_buses()
+        self._generators = self._network.get_generators()
+        self._loads = self._network.get_loads()
+        self._shunts = self._network.get_shunt_compensators()
+        self._lines = self._network.get_lines()
+        self._transfos = self._network.get_2_windings_transformers(all_attributes=True)
+        self._branches = self._network.get_branches()
+        self._slack_terminal = self._network.get_extensions('slackTerminal')
+
+    @property
+    def network(self):
+        return self._network
+
+    @property
+    def buses(self):
+        return self._buses
+
+    @property
+    def generators(self):
+        return self._generators
+
+    @property
+    def loads(self):
+        return self._loads
+
+    @property
+    def shunts(self):
+        return self._shunts
+
+    @property
+    def lines(self):
+        return self._lines
+
+    @property
+    def transformers(self):
+        return self._transfos
+
+    @property
+    def branches(self):
+        return self._branches
+
+    @property
+    def slack_terminal(self):
+        return self._slack_terminal
+
+
+@dataclass
+class VariableContext:
+    gen_p_vars: Any
+    gen_q_vars: Any
+    ph_vars: Any
+    v_vars: Any
+
+
 # pip install pyoptinterface llvmlite tccbox
 #
 # git clone https://github.com/coin-or-tools/ThirdParty-Mumps.git
@@ -117,36 +181,21 @@ class OptimalPowerFlow:
             eq=0.0,
         )
 
-    def run(self):
-        self._network.per_unit = True
-        buses = self._network.get_buses()
-        generators = self._network.get_generators()
-        loads = self._network.get_loads()
-        shunts = self._network.get_shunt_compensators()
-        lines = self._network.get_lines()
-        transfos = self._network.get_2_windings_transformers(all_attributes=True)
-        branches = self._network.get_branches()
-
-        slack_terminal = self._network.get_extensions('slackTerminal')
-        if len(slack_terminal) > 0:
-            slack_bus_id = slack_terminal.iloc[0].bus_id
-        else:
-            slack_bus_id = buses.iloc[0].name
-
+    def create_model(self, network_cache: NetworkCache):
         model = ipopt.Model()
         bf = model.register_function(branch_flow)
         sf = model.register_function(shunt_flow)
-        branch_count = len(lines) + len(transfos)
-        bus_count = len(buses)
-        gen_count = len(generators)
-        shunt_count = len(shunts)
+
+        branch_count = len(network_cache.lines) + len(network_cache.transformers)
+        bus_count = len(network_cache.buses)
+        gen_count = len(network_cache.generators)
+        shunt_count = len(network_cache.shunts)
 
         # create variables
         branch_p1_vars = model.add_variables(range(branch_count), name='branch_p1')
         branch_q1_vars = model.add_variables(range(branch_count), name='branch_q1')
         branch_p2_vars = model.add_variables(range(branch_count), name='branch_p2')
         branch_q2_vars = model.add_variables(range(branch_count), name='branch_q2')
-
         v_vars = model.add_variables(range(bus_count), name="v")
         ph_vars = model.add_variables(range(bus_count), name="ph")
 
@@ -156,23 +205,25 @@ class OptimalPowerFlow:
             model.set_variable_bounds(v_vars[i], vmin, vmax)
 
         # slack bus angle forced to 0
-        slack_bus_num = buses.index.get_loc(slack_bus_id)
+        if len(network_cache.slack_terminal) > 0:
+            slack_bus_id = network_cache.slack_terminal.iloc[0].bus_id
+        else:
+            slack_bus_id = network_cache.buses.iloc[0].name
+        slack_bus_num = network_cache.buses.index.get_loc(slack_bus_id)
         model.set_variable_bounds(ph_vars[slack_bus_num], 0.0, 0.0)
 
         # generators reactive power bounds
         gen_p_vars = model.add_variables(range(gen_count), name="gen_p")
         gen_q_vars = model.add_variables(range(gen_count), name="gen_q")
-
         shunt_p_vars = model.add_variables(range(gen_count), name="shunt_p")
         shunt_q_vars = model.add_variables(range(gen_count), name="shunt_q")
-
         q_margin = 1.0 / self._network.nominal_apparent_power
-        for gen_num, row in enumerate(generators.itertuples(index=False)):
+        for gen_num, row in enumerate(network_cache.generators.itertuples(index=False)):
             model.set_variable_bounds(gen_p_vars[gen_num], row.min_p, row.max_p)
             model.set_variable_bounds(gen_q_vars[gen_num], row.min_q + q_margin, row.max_q - q_margin)
 
         # branch flow nonlinear constraints
-        for branch_num, row in enumerate(lines.itertuples(index=False)):
+        for branch_num, row in enumerate(network_cache.lines.itertuples(index=False)):
             r, x, g1, b1, g2, b2 = row.r, row.x, row.g1, row.b1, row.g2, row.b2
             r1 = 1.0
             a1 = 0.0
@@ -183,8 +234,8 @@ class OptimalPowerFlow:
             q2_var = branch_q2_vars[branch_num]
 
             if row.bus1_id and row.bus2_id:
-                bus1_num = buses.index.get_loc(row.bus1_id)
-                bus2_num = buses.index.get_loc(row.bus2_id)
+                bus1_num = network_cache.buses.index.get_loc(row.bus1_id)
+                bus2_num = network_cache.buses.index.get_loc(row.bus2_id)
                 v1_var = v_vars[bus1_num]
                 v2_var = v_vars[bus2_num]
                 ph1_var = ph_vars[bus1_num]
@@ -194,8 +245,7 @@ class OptimalPowerFlow:
                                                        r, x, g1, b1, g2, b2, r1, a1)
             else:
                 raise PyPowsyblError("Only branches connected to both sides are supported")
-
-        for transfo_num, row in enumerate(transfos.itertuples(index=False)):
+        for transfo_num, row in enumerate(network_cache.transformers.itertuples(index=False)):
             r, x, g, b, rho = row.r, row.x, row.g, row.b, row.rho
             g1 = g / 2
             g2 = g / 2
@@ -204,15 +254,15 @@ class OptimalPowerFlow:
             r1 = rho
             a1 = 0  # TODO
 
-            branch_num = len(lines) + transfo_num
+            branch_num = len(network_cache.lines) + transfo_num
             p1_var = branch_p1_vars[branch_num]
             q1_var = branch_q1_vars[branch_num]
             p2_var = branch_p2_vars[branch_num]
             q2_var = branch_q2_vars[branch_num]
 
             if row.bus1_id and row.bus2_id:
-                bus1_num = buses.index.get_loc(row.bus1_id)
-                bus2_num = buses.index.get_loc(row.bus2_id)
+                bus1_num = network_cache.buses.index.get_loc(row.bus1_id)
+                bus2_num = network_cache.buses.index.get_loc(row.bus2_id)
                 v1_var = v_vars[bus1_num]
                 v2_var = v_vars[bus2_num]
                 ph1_var = ph_vars[bus1_num]
@@ -228,39 +278,35 @@ class OptimalPowerFlow:
         bus_q_gen = [[] for i in range(bus_count)]
         bus_p_load = [0.0 for i in range(bus_count)]
         bus_q_load = [0.0 for i in range(bus_count)]
-
-        for branch_num, row in enumerate(branches.itertuples(index=False)):
+        for branch_num, row in enumerate(network_cache.branches.itertuples(index=False)):
             if row.bus1_id and row.bus2_id:
-                bus1_num = buses.index.get_loc(row.bus1_id)
-                bus2_num = buses.index.get_loc(row.bus2_id)
+                bus1_num = network_cache.buses.index.get_loc(row.bus1_id)
+                bus2_num = network_cache.buses.index.get_loc(row.bus2_id)
                 bus_p_gen[bus1_num].append(branch_p1_vars[branch_num])
                 bus_q_gen[bus1_num].append(branch_q1_vars[branch_num])
                 bus_p_gen[bus2_num].append(branch_p2_vars[branch_num])
                 bus_q_gen[bus2_num].append(branch_q2_vars[branch_num])
             else:
                 raise PyPowsyblError("Only branches connected to both sides are supported")
-
-        for num, row in enumerate(generators.itertuples(index=False)):
+        for num, row in enumerate(network_cache.generators.itertuples(index=False)):
             bus_id = row.bus_id
             if bus_id:
-                bus_num = buses.index.get_loc(bus_id)
+                bus_num = network_cache.buses.index.get_loc(bus_id)
                 bus_p_gen[bus_num].append(gen_p_vars[num])
                 bus_q_gen[bus_num].append(gen_q_vars[num])
-
-        loads_sum = loads.groupby("bus_id", as_index=False).agg({"p0": "sum", "q0": "sum"})
+        loads_sum = network_cache.loads.groupby("bus_id", as_index=False).agg({"p0": "sum", "q0": "sum"})
         for row in loads_sum.itertuples(index=False):
             bus_id = row.bus_id
             if bus_id:
-                bus_num = buses.index.get_loc(bus_id)
+                bus_num = network_cache.buses.index.get_loc(bus_id)
                 bus_p_load[bus_num] -= row.p0
                 bus_q_load[bus_num] -= row.q0
-
-        for num, row in enumerate(shunts.itertuples(index=False)):
+        for num, row in enumerate(network_cache.shunts.itertuples(index=False)):
             g, b, bus_id = row.g, row.b, row.bus_id
             if bus_id:
                 p_var = shunt_p_vars[num]
                 q_var = shunt_q_vars[num]
-                bus_num = buses.index.get_loc(bus_id)
+                bus_num = network_cache.buses.index.get_loc(bus_id)
                 v_var = v_vars[bus_num]
                 model.add_nl_constraint(
                     sf,
@@ -277,7 +323,6 @@ class OptimalPowerFlow:
                 )
                 bus_p_gen[bus_num].append(p_var)
                 bus_q_gen[bus_num].append(q_var)
-
         for bus_num in range(bus_count):
             bus_p_expr = poi.ExprBuilder()
             bus_p_expr += poi.quicksum(bus_p_gen[bus_num])
@@ -296,35 +341,40 @@ class OptimalPowerFlow:
             cost += a * gen_p_vars[gen_num] * gen_p_vars[gen_num] + b * gen_p_vars[gen_num] + c
         model.set_objective(cost)
 
-        model.optimize()
+        return model, VariableContext(gen_p_vars, gen_q_vars, ph_vars, v_vars)
 
-        print(model.get_model_attribute(poi.ModelAttribute.TerminationStatus))
-
-        # update
+    def update_network(self, network_cache: NetworkCache, model, variable_context: VariableContext):
         gen_ids = []
         gen_target_p = []
         gen_target_q = []
         gen_target_v = []
-        for gen_num, (gen_id, row) in enumerate(generators.iterrows()):
+        for gen_num, (gen_id, row) in enumerate(network_cache.generators.iterrows()):
             bus_id = row.bus_id
             if bus_id:
                 gen_ids.append(gen_id)
-                gen_target_p.append(-model.get_value(gen_p_vars[gen_num]))
-                gen_target_q.append(-model.get_value(gen_q_vars[gen_num]))
-                bus_num = buses.index.get_loc(bus_id)
-                gen_target_v.append(model.get_value(v_vars[bus_num]))
-
+                gen_target_p.append(-model.get_value(variable_context.gen_p_vars[gen_num]))
+                gen_target_q.append(-model.get_value(variable_context.gen_q_vars[gen_num]))
+                bus_num = network_cache.buses.index.get_loc(bus_id)
+                gen_target_v.append(model.get_value(variable_context.v_vars[bus_num]))
         self._network.update_generators(id=gen_ids, target_p=gen_target_p, target_q=gen_target_q, target_v=gen_target_v,
                                         voltage_regulator_on=[True] * len(gen_ids))
-        # print(n.get_generators())
 
         bus_ids = []
         bus_v_mag = []
         bus_v_angle = []
-        for bus_num, (bus_id, row) in enumerate(buses.iterrows()):
+        for bus_num, (bus_id, row) in enumerate(network_cache.buses.iterrows()):
             bus_ids.append(bus_id)
-            bus_v_mag.append(model.get_value(v_vars[bus_num]))
-            bus_v_angle.append(model.get_value(ph_vars[bus_num]))
-
+            bus_v_mag.append(model.get_value(variable_context.v_vars[bus_num]))
+            bus_v_angle.append(model.get_value(variable_context.ph_vars[bus_num]))
         self._network.update_buses(id=bus_ids, v_mag=bus_v_mag, v_angle=bus_v_angle)
-        # print(n.get_buses())
+
+    def run(self):
+        network_cache = NetworkCache(self._network)
+
+        model, variable_context = self.create_model(network_cache)
+        model.optimize()
+
+        status = model.get_model_attribute(poi.ModelAttribute.TerminationStatus)
+        logger.info(f"Optimize end with status {status}")
+
+        self.update_network(network_cache, model, variable_context)
--- pypowsybl/test2.py
+++ pypowsybl/test2.py
@@ -9,10 +9,8 @@ from pypowsybl import PyPowsyblError
 from pypowsybl.opf import OptimalPowerFlow
 
 if __name__ == "__main__":
-    import pandas as pd
-
-    pd.options.display.max_columns = None
-    pd.options.display.expand_frame_repr = False
+    logging.basicConfig(level=logging.DEBUG)
+    logging.getLogger('powsybl').setLevel(logging.DEBUG)
 
     n = pp.network.create_ieee118()
 #    n = pp.network.create_eurostag_tutorial_example1_network()
@@ -20,7 +18,5 @@ if __name__ == "__main__":
     opf.run()
 
     parameters = pp.loadflow.Parameters(voltage_init_mode=pp.loadflow.VoltageInitMode.PREVIOUS_VALUES)
-    logging.basicConfig(level=logging.DEBUG)
-    logging.getLogger('powsybl').setLevel(logging.DEBUG)
     r = pp.loadflow.run_ac(n, parameters)
     print(r)
