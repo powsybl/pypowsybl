d5a0ca874f435f841ba4d3472586a15db6cef967
Geoffroy Jamgotchian <geoffroy.jamgotchian@gmail.com>
2025-05-04 22:10:48 +0200

    Refactoring
    Signed-off-by: Geoffroy Jamgotchian <geoffroy.jamgotchian@gmail.com>

--- pypowsybl/opf/impl/network_cache.py
+++ pypowsybl/opf/impl/network_cache.py
@@ -8,15 +8,50 @@ class NetworkCache:
     def __init__(self, network: Network) -> None:
         self._network = network
         self._network.per_unit = True
-        self._voltage_levels = self._network.get_voltage_levels(attributes=['low_voltage_limit', 'high_voltage_limit'])
-        self._buses = pd.merge(self._network.get_buses(attributes=['voltage_level_id']), self._voltage_levels, left_on='voltage_level_id', right_index=True, how='left')
-        self._generators = self._network.get_generators(attributes=['bus_id', 'min_p', 'max_p', 'min_q_at_target_p', 'max_q_at_target_p', 'target_p'])
-        self._loads = self._network.get_loads(attributes=['bus_id', 'p0', 'q0'])
-        self._shunts = self._network.get_shunt_compensators(attributes=['bus_id', 'g', 'b'])
-        self._lines = self._network.get_lines(attributes=['bus1_id', 'bus2_id', 'r', 'x', 'g1', 'g2', 'b1', 'b2'])
-        self._transformers = self._network.get_2_windings_transformers(attributes=['bus1_id', 'bus2_id', 'rho', 'alpha', 'r_tap', 'x_tap', 'g_tap', 'b_tap'])
-        self._branches = self._network.get_branches(attributes=['bus1_id', 'bus2_id'])
+        self._voltage_levels = self._build_voltage_levels(network)
+        self._buses = self._build_buses(network, self._voltage_levels)
+        self._generators = self._build_generators(network)
+        self._loads = self._build_loads(network)
+        self._shunts = self._build_shunt_compensators(network)
+        self._lines = self._build_lines(network)
+        self._transformers = self._build_2_windings_transformers(network)
+        self._branches = self._build_branches(network)
         self._slack_terminal = self._network.get_extensions('slackTerminal')
+
+    @staticmethod
+    def _build_branches(network: Network):
+        return network.get_branches(attributes=['bus1_id', 'bus2_id'])
+
+    @staticmethod
+    def _build_2_windings_transformers(network: Network):
+        return network.get_2_windings_transformers(
+            attributes=['bus1_id', 'bus2_id', 'rho', 'alpha', 'r_tap', 'x_tap', 'g_tap', 'b_tap'])
+
+    @staticmethod
+    def _build_lines(network: Network):
+        return network.get_lines(attributes=['bus1_id', 'bus2_id', 'r', 'x', 'g1', 'g2', 'b1', 'b2'])
+
+    @staticmethod
+    def _build_shunt_compensators(network: Network):
+        return network.get_shunt_compensators(attributes=['bus_id', 'g', 'b'])
+
+    @staticmethod
+    def _build_loads(network: Network):
+        return network.get_loads(attributes=['bus_id', 'p0', 'q0'])
+
+    @staticmethod
+    def _build_generators(network: Network):
+        return network.get_generators(
+            attributes=['bus_id', 'min_p', 'max_p', 'min_q_at_target_p', 'max_q_at_target_p', 'target_p'])
+
+    @staticmethod
+    def _build_buses(network: Network, voltage_levels: DataFrame):
+        return pd.merge(network.get_buses(attributes=['voltage_level_id']), voltage_levels,
+                        left_on='voltage_level_id', right_index=True, how='left')
+
+    @staticmethod
+    def _build_voltage_levels(network: Network) -> DataFrame:
+        return network.get_voltage_levels(attributes=['low_voltage_limit', 'high_voltage_limit'])
 
     @property
     def network(self) -> Network:
