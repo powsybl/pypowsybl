97cf85ce9a2a7e1010b6fefcea965aec0d458932
Geoffroy Jamgotchian <geoffroy.jamgotchian@gmail.com>
2025-05-04 15:44:58 +0200

    Refactor
    Signed-off-by: Geoffroy Jamgotchian <geoffroy.jamgotchian@gmail.com>

--- pypowsybl/opf/impl/opf.py
+++ pypowsybl/opf/impl/opf.py
@@ -261,10 +261,7 @@ class OptimalPowerFlow:
             model.set_variable_start(variable_context.v_vars[bus_num], 1.0)
 
         # slack bus angle forced to 0
-        if len(network_cache.slack_terminal) > 0:
-            slack_bus_id = network_cache.slack_terminal.iloc[0].bus_id
-        else:
-            slack_bus_id = network_cache.buses.iloc[0].name
+        slack_bus_id = network_cache.slack_terminal.iloc[0].bus_id if len(network_cache.slack_terminal) > 0 else network_cache.buses.iloc[0].name
         slack_bus_num = network_cache.buses.index.get_loc(slack_bus_id)
         model.set_variable_bounds(variable_context.ph_vars[slack_bus_num], 0.0, 0.0)
         logger.log(TRACE_LEVEL, f"Angle reference is at bus '{slack_bus_id}' (num={slack_bus_num})")
