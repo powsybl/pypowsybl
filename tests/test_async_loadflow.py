8552785e4732b248b97609ba7566deebc2743659
Geoffroy Jamgotchian <geoffroy.jamgotchian@rte-france.com>
2025-05-23 11:18:10 +0200

    Add more test
    Signed-off-by: Geoffroy Jamgotchian <geoffroy.jamgotchian@rte-france.com>

--- tests/test_async_loadflow.py
+++ tests/test_async_loadflow.py
@@ -4,15 +4,21 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 #
+import asyncio
+import logging
+
 import pytest
 
 import pypowsybl as pp
 import pypowsybl.loadflow as lf
+from pypowsybl.network import Network
 
 
 @pytest.fixture(autouse=True)
 def set_up():
     pp.set_config_read(False)
+    logging.basicConfig()
+    logging.getLogger('powsybl').setLevel(logging.INFO)
 
 
 @pytest.mark.asyncio
@@ -29,3 +35,31 @@ async def test_run_lf_ac_async():
     assert 'VL1_0' == results[0].slack_bus_results[0].id
     assert abs(results[0].slack_bus_results[0].active_power_mismatch) < 0.01
     assert 3 == results[0].iteration_count
+
+
+def scale_loads(network: Network, variant_id: str):
+    network.set_working_variant(variant_id)
+    loads = network.get_loads(attributes=['p0', 'q0'])
+    loads['p0'] = loads['p0'] + 1
+    loads['q0'] = loads['q0'] + 1
+    network.update_loads(loads)
+
+
+@pytest.mark.asyncio
+async def test_run_lf_ac_async_multi_variants():
+    network = pp.network.create_ieee14()
+
+    task_count = 3
+    for task_num in range(task_count):
+        network.clone_variant("InitialState", f"v{task_num}")
+
+    results = []
+    for task_num in range(task_count):
+        variant_id = f"v{task_num}"
+        scale_loads(network, variant_id)
+        results.append(pp.loadflow.run_ac_async(network, variant_id))
+
+    await asyncio.gather(*results)
+
+    for task_num in range(task_count):
+        network.remove_variant(f"v{task_num}")
