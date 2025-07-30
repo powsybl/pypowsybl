c93ac705525b055293bb1eba9f01f4eeb8fd6f15
Godelaine de Montmorillon <godelaine.demontmorillon@rte-france.com>
2025-07-30 17:26:45 +0200

    define in cpp
    Signed-off-by: Godelaine de Montmorillon <godelaine.demontmorillon@rte-france.com>

--- cpp/powsybl-cpp/powsybl-cpp.h
+++ cpp/powsybl-cpp/powsybl-cpp.h
@@ -582,6 +582,8 @@ public:
 
     std::vector<std::string> provider_parameters_keys;
     std::vector<std::string> provider_parameters_values;
+
+    float shifted_violation_penalty;
 };
 
 RaoParameters* createRaoParameters();
