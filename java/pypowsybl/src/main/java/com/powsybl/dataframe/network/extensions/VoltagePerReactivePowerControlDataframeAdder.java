c41172077e6164be51b5619e2c36ca4646b314ac
Hugo KULESZA <hugo.kulesza@rte-france.com>
2025-09-09 11:23:52 +0200

    Adding static var compensator extension
    Signed-off-by: Hugo KULESZA <hugo.kulesza@rte-france.com>

---
+++ java/pypowsybl/src/main/java/com/powsybl/dataframe/network/extensions/VoltagePerReactivePowerControlDataframeAdder.java
@@ -0,0 +1,60 @@
+package com.powsybl.dataframe.network.extensions;
+
+import com.powsybl.commons.PowsyblException;
+import com.powsybl.dataframe.SeriesMetadata;
+import com.powsybl.dataframe.network.adders.AbstractSimpleAdder;
+import com.powsybl.dataframe.network.adders.SeriesUtils;
+import com.powsybl.dataframe.update.DoubleSeries;
+import com.powsybl.dataframe.update.StringSeries;
+import com.powsybl.dataframe.update.UpdatingDataframe;
+import com.powsybl.iidm.network.Network;
+import com.powsybl.iidm.network.StaticVarCompensator;
+import com.powsybl.iidm.network.extensions.VoltagePerReactivePowerControlAdder;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * @author Hugo Kulesza {@literal <hugo.kulesza at rte-france.com>}
+ */
+public class VoltagePerReactivePowerControlDataframeAdder extends AbstractSimpleAdder {
+
+    private static final List<SeriesMetadata> METADATA = List.of(
+            SeriesMetadata.stringIndex("id"),
+            SeriesMetadata.doubles("slope")
+    );
+
+    @Override
+    public List<List<SeriesMetadata>> getMetadata() {
+        return Collections.singletonList(METADATA);
+    }
+
+    private static class VoltagePerReactivePowerControlSeries {
+        private final StringSeries id;
+        private final DoubleSeries slope;
+
+        VoltagePerReactivePowerControlSeries(UpdatingDataframe dataframe) {
+            this.id = dataframe.getStrings("id");
+            this.slope = dataframe.getDoubles("slope");
+        }
+
+        void create(Network network, int row) {
+            String id = this.id.get(row);
+            StaticVarCompensator svc = network.getStaticVarCompensator(id);
+            if (svc == null) {
+                throw new PowsyblException("Invalid static var compensator id : could not find " + id);
+            }
+            VoltagePerReactivePowerControlAdder adder = svc.newExtension(VoltagePerReactivePowerControlAdder.class);
+            SeriesUtils.applyIfPresent(slope, row, adder::withSlope);
+            adder.add();
+        }
+    }
+
+    @Override
+    public void addElements(Network network, UpdatingDataframe dataframe) {
+        VoltagePerReactivePowerControlSeries series = new VoltagePerReactivePowerControlSeries(dataframe);
+        for (int row = 0; row < dataframe.getRowCount(); row++) {
+            series.create(network, row);
+        }
+    }
+}
---
+++ java/pypowsybl/src/main/java/com/powsybl/dataframe/network/extensions/VoltagePerReactivePowerControlDataframeProvider.java
@@ -0,0 +1,73 @@
+package com.powsybl.dataframe.network.extensions;
+
+import com.google.auto.service.AutoService;
+import com.powsybl.commons.PowsyblException;
+import com.powsybl.dataframe.network.ExtensionInformation;
+import com.powsybl.dataframe.network.NetworkDataframeMapper;
+import com.powsybl.dataframe.network.NetworkDataframeMapperBuilder;
+import com.powsybl.dataframe.network.adders.NetworkElementAdder;
+import com.powsybl.iidm.network.Network;
+import com.powsybl.iidm.network.StaticVarCompensator;
+import com.powsybl.iidm.network.extensions.VoltagePerReactivePowerControl;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Stream;
+
+/**
+ * @author Hugo Kulesza {@literal <hugo.kulesza at rte-france.com>}
+ */
+@AutoService(NetworkExtensionDataframeProvider.class)
+public class VoltagePerReactivePowerControlDataframeProvider extends AbstractSingleDataframeNetworkExtension {
+    @Override
+    public String getExtensionName() {
+        return VoltagePerReactivePowerControl.NAME;
+    }
+
+    @Override
+    public ExtensionInformation getExtensionInformation() {
+        return new ExtensionInformation(VoltagePerReactivePowerControl.NAME,
+                "Models the voltage control static var compensators",
+                "index : id (str), slope (float)");
+    }
+
+    private Stream<VoltagePerReactivePowerControl> itemsStream(Network network) {
+        return network.getStaticVarCompensatorStream()
+                .map(svc -> (VoltagePerReactivePowerControl) svc.getExtension(VoltagePerReactivePowerControl.class))
+                .filter(Objects::nonNull);
+    }
+
+    private VoltagePerReactivePowerControl getOrThrow(Network network, String id) {
+        StaticVarCompensator svc = network.getStaticVarCompensator(id);
+        if (svc == null) {
+            throw new PowsyblException("Static var compensator '" + id + "' not found");
+        }
+        VoltagePerReactivePowerControl ext = svc.getExtension(VoltagePerReactivePowerControl.class);
+        if (ext == null) {
+            throw new PowsyblException("Static var compensator '" + id + "' has no VoltagePerReactivePowerControl extension");
+        }
+        return ext;
+    }
+
+    @Override
+    public NetworkDataframeMapper createMapper() {
+        return NetworkDataframeMapperBuilder.ofStream(this::itemsStream, this::getOrThrow)
+                .stringsIndex("id", ext -> ext.getExtendable().getId())
+                .doubles("slope", VoltagePerReactivePowerControl::getSlope, (ext, val, context) -> ext.setSlope(val))
+                .build();
+    }
+
+    @Override
+    public void removeExtensions(Network network, List<String> ids) {
+        ids.stream().filter(Objects::nonNull)
+                .map(network::getStaticVarCompensator)
+                .filter(Objects::nonNull)
+                .forEach(svc -> svc.removeExtension(VoltagePerReactivePowerControl.class));
+    }
+
+    @Override
+    public NetworkElementAdder createAdder() {
+        return new VoltagePerReactivePowerControlDataframeAdder();
+    }
+
+}
