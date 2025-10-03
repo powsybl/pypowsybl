506807ed318ec4c2befa3724ddd9f463ce0772ec
lisrte <laurent.issertial_externe@rte-france.com>
2025-10-03 17:33:47 +0200

    EventMapping and OutputVariableMapping in dynamic simulation runner set to optional
    Signed-off-by: lisrte <laurent.issertial_externe@rte-france.com>

--- integration_tests/test_dynawo.py
+++ integration_tests/test_dynawo.py
@@ -11,6 +11,7 @@ import pypowsybl.report as rp
 from pypowsybl._pypowsybl import DynamicSimulationStatus
 import pandas as pd
 from pathlib import Path
+import logging
 
 def test_simulation():
     """
@@ -70,7 +71,8 @@ def test_minimal_simulation():
     and configured its path in your config.yml.
     """
     network = pp.network.create_ieee14()
-
+    logging.basicConfig()
+    logging.getLogger('powsybl').setLevel(logging.DEBUG)
     model_mapping = dyn.ModelMapping()
     generator_mapping_df = pd.DataFrame(
         index=pd.Series(name='static_id', data=['B6-G', 'B8-G']),
@@ -80,9 +82,6 @@ def test_minimal_simulation():
         }
     )
     model_mapping.add_synchronous_generator(generator_mapping_df)
-
-    event_mapping = dyn.EventMapping()
-    variables_mapping = dyn.OutputVariableMapping()
 
     testPath = Path(__file__).parent
     dynawo_param = {
@@ -96,7 +95,7 @@ def test_minimal_simulation():
     param = dyn.Parameters(start_time=0, stop_time=100, provider_parameters=dynawo_param)
 
     sim = dyn.Simulation()
-    res = sim.run(network, model_mapping, event_mapping, variables_mapping, param)
+    res = sim.run(network=network, model_mapping=model_mapping, parameters=param)
 
     assert DynamicSimulationStatus.SUCCESS == res.status()
     assert "" == res.status_text()
