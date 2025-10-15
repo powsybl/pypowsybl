ea6676a9e08f858e70769e0942b57495a66be389
Florian Dupuy <66690739+flo-dup@users.noreply.github.com>
2025-10-15 13:41:10 +0200

    Fix injectionsAdded parameter not taken into account (#1073)
    Signed-off-by: Florian Dupuy <florian.dupuy@rte-france.com>

--- java/pypowsybl/src/main/java/com/powsybl/python/network/NetworkCFunctions.java
+++ java/pypowsybl/src/main/java/com/powsybl/python/network/NetworkCFunctions.java
@@ -1237,6 +1237,8 @@ public final class NetworkCFunctions {
                 .setBusLegend(nadParametersPointer.isBusLegend())
                 .setSubstationDescriptionDisplayed(nadParametersPointer.isSubstationDescriptionDisplayed())
                 .setEdgeInfoDisplayed(edgeInfo);
+        nadParameters.getLayoutParameters()
+                .setInjectionsAdded(nadParametersPointer.isInjectionsAdded());
         return nadParameters;
     }
 
