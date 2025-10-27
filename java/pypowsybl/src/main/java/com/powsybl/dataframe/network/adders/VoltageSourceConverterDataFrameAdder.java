5b28d43e220b871e13dfac2abcbacc704cb417bb
Denis Bonnand <denis.bonnand@supergrid-institute.com>
2025-10-27 14:13:29 +0100

    Add author specification
    Signed-off-by: Denis Bonnand <denis.bonnand@supergrid-institute.com>

--- java/pypowsybl/src/main/java/com/powsybl/dataframe/network/adders/VoltageSourceConverterDataFrameAdder.java
+++ java/pypowsybl/src/main/java/com/powsybl/dataframe/network/adders/VoltageSourceConverterDataFrameAdder.java
@@ -1,3 +1,10 @@
+/**
+ * Copyright (c) 2025, SuperGrid Institute (https://www.supergrid-institute.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * SPDX-License-Identifier: MPL-2.0
+ */
 package com.powsybl.dataframe.network.adders;
 
 import com.powsybl.commons.PowsyblException;
@@ -16,6 +23,9 @@ import static com.powsybl.dataframe.network.adders.NetworkUtils.getVoltageLevelO
 import static com.powsybl.dataframe.network.adders.SeriesUtils.applyBooleanIfPresent;
 import static com.powsybl.dataframe.network.adders.SeriesUtils.applyIfPresent;
 
+/**
+ * @author Denis BONNAND {@literal <denis.bonnand at supergrid-institute.com>}
+ */
 public class VoltageSourceConverterDataFrameAdder extends AbstractSimpleAdder {
 
     private static final List<SeriesMetadata> METADATA = List.of(
