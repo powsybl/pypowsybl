4e982130ed1272c941b4725f803cf3323a353251
Denis Bonnand <denis.bonnand@supergrid-institute.com>
2025-10-27 14:13:29 +0100

    Add author specification
    Signed-off-by: Denis Bonnand <denis.bonnand@supergrid-institute.com>

--- java/pypowsybl/src/main/java/com/powsybl/dataframe/network/adders/DcLineSeries.java
+++ java/pypowsybl/src/main/java/com/powsybl/dataframe/network/adders/DcLineSeries.java
@@ -1,3 +1,10 @@
+/**
+ * Copyright (c) 2025, SuperGrid Institute (https://www.supergrid-institute.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * SPDX-License-Identifier: MPL-2.0
+ */
 package com.powsybl.dataframe.network.adders;
 
 import com.powsybl.dataframe.update.DoubleSeries;
@@ -10,6 +17,9 @@ import java.util.Optional;
 
 import static com.powsybl.dataframe.network.adders.SeriesUtils.applyIfPresent;
 
+/**
+ * @author Denis BONNAND {@literal <denis.bonnand at supergrid-institute.com>}
+ */
 public class DcLineSeries extends IdentifiableSeries {
 
     protected final StringSeries dcNodes1;
