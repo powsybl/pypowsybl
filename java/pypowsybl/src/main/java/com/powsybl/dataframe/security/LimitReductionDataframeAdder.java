00a06e6e456b01bed9a58a5bdb0d2545d7181f5c
Hugo KULESZA <hugo.kulesza@rte-france.com>
2025-12-09 14:02:39 +0100

    Add documentation + headers
    Signed-off-by: Hugo KULESZA <hugo.kulesza@rte-france.com>

--- java/pypowsybl/src/main/java/com/powsybl/dataframe/security/LimitReductionDataframeAdder.java
+++ java/pypowsybl/src/main/java/com/powsybl/dataframe/security/LimitReductionDataframeAdder.java
@@ -1,3 +1,10 @@
+/**
+ * Copyright (c) 2025, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * SPDX-License-Identifier: MPL-2.0
+ */
 package com.powsybl.dataframe.security;
 
 import com.powsybl.contingency.ContingencyContext;
@@ -24,6 +31,9 @@ import com.powsybl.security.limitreduction.LimitReduction;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * @author Hugo Kulesza {@literal <hugo.kulesza at rte-france.com>}
+ */
 public class LimitReductionDataframeAdder {
 
     private static final List<SeriesMetadata> METADATA = List.of(
