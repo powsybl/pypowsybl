914d37f1c4c67ccc0c00e4554d15a614d1def007
Bertrand Rix <bertrand.rix@artelys.com>
2026-03-17 16:43:53 +0100

    Return type
    Signed-off-by: Bertrand Rix <bertrand.rix@artelys.com>

--- pypowsybl/rao/impl/crac.py
+++ pypowsybl/rao/impl/crac.py
@@ -10,6 +10,8 @@ from pypowsybl import _pypowsybl
 from pypowsybl.network import Network
 from pypowsybl.utils import path_to_str
 from pypowsybl.utils import create_data_frame_from_series_array
+from pandas import DataFrame
+
 from os import PathLike
 
 from typing import (
@@ -39,7 +41,7 @@ class Crac:
     Returns:
         A dataframe containing the crac instants
     """
-    def get_crac_instants(self):
+    def get_crac_instants(self) -> DataFrame:
         series = _pypowsybl.get_crac_instants(self._handle)
         return create_data_frame_from_series_array(series)
 
@@ -49,7 +51,7 @@ class Crac:
     Returns:
         A dataframe containing the max remedial actions usage limit
     """
-    def get_max_remedial_actions_usage_limit(self):
+    def get_max_remedial_actions_usage_limit(self) -> DataFrame:
         series = _pypowsybl.get_max_remedial_actions_usage_limit(self._handle)
         return create_data_frame_from_series_array(series)
 
@@ -59,7 +61,7 @@ class Crac:
     Returns:
         A dataframe containing the max tso usage limit
     """
-    def get_max_tso_usage_limit(self):
+    def get_max_tso_usage_limit(self) -> DataFrame:
         series = _pypowsybl.get_max_tso_usage_limit(self._handle)
         return create_data_frame_from_series_array(series)
 
@@ -69,7 +71,7 @@ class Crac:
     Returns:
         A dataframe containing the max topological action per tso usage limit
     """
-    def get_max_topological_action_per_tso_usage_limit(self):
+    def get_max_topological_action_per_tso_usage_limit(self) -> DataFrame:
         series = _pypowsybl.get_max_topological_action_per_tso_usage_limit(self._handle)
         return create_data_frame_from_series_array(series)
 
@@ -79,7 +81,7 @@ class Crac:
     Returns:
         A dataframe containing the max pst action per tso usage limit
     """
-    def get_max_pst_action_per_tso_usage_limit(self):
+    def get_max_pst_action_per_tso_usage_limit(self) -> DataFrame:
         series = _pypowsybl.get_max_pst_action_per_tso_usage_limit(self._handle)
         return create_data_frame_from_series_array(series)
 
@@ -89,7 +91,7 @@ class Crac:
     Returns:
         A dataframe containing the max remedial action per tso usage limit
     """
-    def get_max_remedial_action_per_tso_usage_limit(self):
+    def get_max_remedial_action_per_tso_usage_limit(self) -> DataFrame:
         series = _pypowsybl.get_max_remedial_action_per_tso_usage_limit(self._handle)
         return create_data_frame_from_series_array(series)
 
@@ -99,7 +101,7 @@ class Crac:
     Returns:
         A dataframe containing the max elementary action per tso usage limit
     """
-    def get_max_elementary_action_per_tso_usage_limit(self):
+    def get_max_elementary_action_per_tso_usage_limit(self) -> DataFrame:
         series = _pypowsybl.get_max_elementary_action_per_tso_usage_limit(self._handle)
         return create_data_frame_from_series_array(series)
 
@@ -109,7 +111,7 @@ class Crac:
     Returns:
         A dataframe containing the crac contingencies
     """
-    def get_crac_contingencies(self):
+    def get_crac_contingencies(self) -> DataFrame:
         series = _pypowsybl.get_crac_contingencies(self._handle)
         return create_data_frame_from_series_array(series)
 
@@ -119,7 +121,7 @@ class Crac:
     Returns:
         A dataframe containing the crac contingency elements
     """
-    def get_crac_contingency_elements(self):
+    def get_crac_contingency_elements(self) -> DataFrame:
         series = _pypowsybl.get_crac_contingency_elements(self._handle)
         return create_data_frame_from_series_array(series)
 
@@ -129,7 +131,7 @@ class Crac:
     Returns:
         A dataframe containing the crac flow cnecs
     """
-    def get_crac_flow_cnecs(self):
+    def get_crac_flow_cnecs(self) -> DataFrame:
         series = _pypowsybl.get_crac_flow_cnecs(self._handle)
         return create_data_frame_from_series_array(series)
 
@@ -139,7 +141,7 @@ class Crac:
     Returns:
         A dataframe containing the crac angle cnecs
     """
-    def get_crac_angle_cnecs(self):
+    def get_crac_angle_cnecs(self) -> DataFrame:
         series = _pypowsybl.get_crac_angle_cnecs(self._handle)
         return create_data_frame_from_series_array(series)
 
@@ -149,7 +151,7 @@ class Crac:
     Returns:
         A dataframe containing the crac voltage cnecs
     """
-    def get_crac_voltage_cnecs(self):
+    def get_crac_voltage_cnecs(self) -> DataFrame:
         series = _pypowsybl.get_crac_voltage_cnecs(self._handle)
         return create_data_frame_from_series_array(series)
 
@@ -159,7 +161,7 @@ class Crac:
     Returns:
         A dataframe containing the crac thresholds
     """
-    def get_crac_thresholds(self):
+    def get_crac_thresholds(self) -> DataFrame:
         series = _pypowsybl.get_crac_thresholds(self._handle)
         return create_data_frame_from_series_array(series)
 
@@ -169,7 +171,7 @@ class Crac:
     Returns:
         A dataframe containing the crac pst range actions
     """
-    def get_crac_pst_range_actions(self):
+    def get_crac_pst_range_actions(self) -> DataFrame:
         series = _pypowsybl.get_crac_pst_range_actions(self._handle)
         return create_data_frame_from_series_array(series)
 
@@ -179,7 +181,7 @@ class Crac:
     Returns:
         A dataframe containing the crac hvdc range actions
     """
-    def get_crac_hvdc_range_actions(self):
+    def get_crac_hvdc_range_actions(self) -> DataFrame:
         series = _pypowsybl.get_crac_hvdc_range_actions(self._handle)
         return create_data_frame_from_series_array(series)
 
@@ -189,7 +191,7 @@ class Crac:
     Returns:
         A dataframe containing the crac injection range actions
     """
-    def get_crac_injection_range_actions(self):
+    def get_crac_injection_range_actions(self) -> DataFrame:
         series = _pypowsybl.get_crac_injection_range_actions(self._handle)
         return create_data_frame_from_series_array(series)
 
@@ -199,7 +201,7 @@ class Crac:
     Returns:
         A dataframe containing the crac injection ra elements
     """
-    def get_crac_injection_ra_elements(self):
+    def get_crac_injection_ra_elements(self) -> DataFrame:
         series = _pypowsybl.get_crac_injection_ra_elements(self._handle)
         return create_data_frame_from_series_array(series)
 
@@ -209,7 +211,7 @@ class Crac:
     Returns:
         A dataframe containing the crac counter trade range actions
     """
-    def get_crac_counter_trade_range_actions(self):
+    def get_crac_counter_trade_range_actions(self) -> DataFrame:
         series = _pypowsybl.get_crac_counter_trade_range_actions(self._handle)
         return create_data_frame_from_series_array(series)
 
@@ -219,7 +221,7 @@ class Crac:
     Returns:
         A dataframe containing the crac range action ranges
     """
-    def get_crac_range_action_ranges(self):
+    def get_crac_range_action_ranges(self) -> DataFrame:
         series = _pypowsybl.get_crac_range_action_ranges(self._handle)
         return create_data_frame_from_series_array(series)
 
@@ -229,7 +231,7 @@ class Crac:
     Returns:
         A dataframe containing the crac network actions
     """
-    def get_crac_network_actions(self):
+    def get_crac_network_actions(self) -> DataFrame:
         series = _pypowsybl.get_crac_network_actions(self._handle)
         return create_data_frame_from_series_array(series)
 
@@ -239,7 +241,7 @@ class Crac:
     Returns:
         A dataframe containing the terminal connection actions
     """
-    def get_terminal_connection_actions(self):
+    def get_terminal_connection_actions(self) -> DataFrame:
         series = _pypowsybl.get_terminal_connection_actions(self._handle)
         return create_data_frame_from_series_array(series)
 
@@ -249,7 +251,7 @@ class Crac:
     Returns:
         A dataframe containing the pst tap position actions
     """
-    def get_pst_tap_position_actions(self):
+    def get_pst_tap_position_actions(self) -> DataFrame:
         series = _pypowsybl.get_pst_tap_position_actions(self._handle)
         return create_data_frame_from_series_array(series)
 
@@ -259,7 +261,7 @@ class Crac:
     Returns:
         A dataframe containing the generator actions
     """
-    def get_generator_actions(self):
+    def get_generator_actions(self) -> DataFrame:
         series = _pypowsybl.get_generator_actions(self._handle)
         return create_data_frame_from_series_array(series)
 
@@ -269,7 +271,7 @@ class Crac:
     Returns:
         A dataframe containing the load actions
     """
-    def get_load_actions(self):
+    def get_load_actions(self) -> DataFrame:
         series = _pypowsybl.get_load_actions(self._handle)
         return create_data_frame_from_series_array(series)
 
@@ -279,7 +281,7 @@ class Crac:
     Returns:
         A dataframe containing the dangling line actions
     """
-    def get_dangling_line_actions(self):
+    def get_dangling_line_actions(self) -> DataFrame:
         series = _pypowsybl.get_dangling_line_actions(self._handle)
         return create_data_frame_from_series_array(series)
 
@@ -289,7 +291,7 @@ class Crac:
     Returns:
         A dataframe containing the shunt compensator position actions
     """
-    def get_shunt_compensator_position_actions(self):
+    def get_shunt_compensator_position_actions(self) -> DataFrame:
         series = _pypowsybl.get_shunt_compensator_position_actions(self._handle)
         return create_data_frame_from_series_array(series)
 
@@ -299,7 +301,7 @@ class Crac:
     Returns:
         A dataframe containing the switch actions
     """
-    def get_switch_actions(self):
+    def get_switch_actions(self) -> DataFrame:
         series = _pypowsybl.get_switch_actions(self._handle)
         return create_data_frame_from_series_array(series)
 
@@ -309,7 +311,7 @@ class Crac:
     Returns:
         A dataframe containing the switch pairs action
     """
-    def get_switch_pairs_action(self):
+    def get_switch_pairs_action(self) -> DataFrame:
         series = _pypowsybl.get_switch_pairs_action(self._handle)
         return create_data_frame_from_series_array(series)
 
@@ -319,7 +321,7 @@ class Crac:
     Returns:
         A dataframe containing the on instant usage rule
     """
-    def get_on_instant_usage_rule(self):
+    def get_on_instant_usage_rule(self) -> DataFrame:
         series = _pypowsybl.get_on_instant_usage_rule(self._handle)
         return create_data_frame_from_series_array(series)
 
@@ -329,7 +331,7 @@ class Crac:
     Returns:
         A dataframe containing the on contingency state usage rule
     """
-    def get_on_contingency_state_usage_rule(self):
+    def get_on_contingency_state_usage_rule(self) -> DataFrame:
         series = _pypowsybl.get_on_contingency_state_usage_rule(self._handle)
         return create_data_frame_from_series_array(series)
 
@@ -339,7 +341,7 @@ class Crac:
     Returns:
         A dataframe containing the on constraint usage rule
     """
-    def get_on_constraint_usage_rule(self):
+    def get_on_constraint_usage_rule(self) -> DataFrame:
         series = _pypowsybl.get_on_constraint_usage_rule(self._handle)
         return create_data_frame_from_series_array(series)
 
@@ -349,6 +351,6 @@ class Crac:
     Returns:
         A dataframe containing the on flow constrain in country usage rule
     """
-    def get_on_flow_constraint_in_country_usage_rule(self):
+    def get_on_flow_constraint_in_country_usage_rule(self) -> DataFrame:
         series = _pypowsybl.get_on_flow_constraint_in_country_usage_rule(self._handle)
         return create_data_frame_from_series_array(series)
