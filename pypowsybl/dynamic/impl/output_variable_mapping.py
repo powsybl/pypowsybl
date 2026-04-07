0305c00df9f733ae21811a26642fb6032ce46ced
lisrte <laurent.issertial_externe@rte-france.com>
2026-04-07 11:07:53 +0200

    Fix deprecated documentation
    Signed-off-by: lisrte <laurent.issertial_externe@rte-france.com>

--- pypowsybl/dynamic/impl/output_variable_mapping.py
+++ pypowsybl/dynamic/impl/output_variable_mapping.py
@@ -34,17 +34,17 @@ class OutputVariableMapping:
     def add_dynamic_model_curves(self, dynamic_model_id: str, variables: Union[List[str], str]) -> None:
         """
         .. deprecated:: 1.15.0
-            Use `add_model_curves` instead.
+            Use `add_curves` instead.
         """
-        warnings.warn("add_dynamic_model_curves is deprecated, use add_model_curves instead", DeprecationWarning)
+        warnings.warn("add_dynamic_model_curves is deprecated, use add_curves instead", DeprecationWarning)
         self.add_curves(dynamic_model_id, variables)
 
     def add_standard_model_curves(self, static_id: str, variables: Union[List[str], str]) -> None:
         """
         .. deprecated:: 1.15.0
-            Use :func:`add_model_curves` instead.
+            Use :func:`add_curves` instead.
         """
-        warnings.warn("add_standard_model_curves is deprecated, use add_model_curves instead", DeprecationWarning)
+        warnings.warn("add_standard_model_curves is deprecated, use add_curves instead", DeprecationWarning)
         self.add_curves(static_id, variables)
 
     def add_final_state_values(self, model_id: str, variables: Union[List[str], str]) -> None:
@@ -61,17 +61,17 @@ class OutputVariableMapping:
     def add_dynamic_model_final_state_values(self, dynamic_model_id: str, variables: Union[List[str], str]) -> None:
         """
         .. deprecated:: 1.15.0
-            Use :func:`add_model_final_state_values` instead.
+            Use :func:`add_final_state_values` instead.
         """
-        warnings.warn("add_dynamic_model_final_state_values is deprecated, use add_model_final_state_values instead", DeprecationWarning)
+        warnings.warn("add_dynamic_model_final_state_values is deprecated, use add_final_state_values instead", DeprecationWarning)
         self.add_final_state_values(dynamic_model_id, variables)
 
     def add_standard_model_final_state_values(self, static_id: str, variables: Union[List[str], str]) -> None:
         """
         .. deprecated:: 1.15.0
-            Use :func:`add_model_final_state_values` instead.
+            Use :func:`add_final_state_values` instead.
         """
-        warnings.warn("add_standard_model_final_state_values is deprecated, use add_model_final_state_values instead", DeprecationWarning)
+        warnings.warn("add_standard_model_final_state_values is deprecated, use add_final_state_values instead", DeprecationWarning)
         self.add_final_state_values(static_id, variables)
 
     def _add_output_variables(self, element_id: str, variables: List[str], output_variable_type: OutputVariableType) -> None:
