5236557bfccd5048965287060196e4af7d54b6be
Hugo KULESZA <hugo.kulesza@rte-france.com>
2026-04-09 11:07:23 +0200

    add license + rework imports
    Signed-off-by: Hugo KULESZA <hugo.kulesza@rte-france.com>

--- pypowsybl/network/scalable/impl/updown.py
+++ pypowsybl/network/scalable/impl/updown.py
@@ -1,5 +1,10 @@
-from __future__ import annotations
-
+#
+# Copyright (c) 2026, RTE (http://www.rte-france.com)
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+# SPDX-License-Identifier: MPL-2.0
+#
 from typing import Optional
 
 from .element import ElementScalable
