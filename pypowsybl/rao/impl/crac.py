41f443d0b4b58a9ec79a8463eaf2539a9e7095a3
Bertrand Rix <bertrand.rix@artelys.com>
2026-04-30 11:39:00 +0200

    Appease check type
    Signed-off-by: Bertrand Rix <bertrand.rix@artelys.com>

--- pypowsybl/rao/impl/crac.py
+++ pypowsybl/rao/impl/crac.py
@@ -37,9 +37,11 @@ class Crac:
         return Crac.from_buffer_source(network, io.BytesIO(open(path_to_str(crac_file), "rb").read()), path_to_str(crac_file), creation_parameters_source)
 
     @classmethod
-    def from_buffer_source(cls, network: Network, crac_source: io.BytesIO, crac_file_name: Optional[str] = 'crac.json', parameters_source: Optional[io.BytesIO] = None) -> Any :
+    def from_buffer_source(cls, network: Network, crac_source: io.BytesIO, crac_file_name: Optional[str], parameters_source: Optional[io.BytesIO] = None) -> Any :
         if parameters_source is None:
             parameters_source = io.BytesIO(b"{\"crac-factory\": \"CracImplFactory\"}")
+        if crac_file_name is None:
+            crac_file_name = 'crac.json'
         return cls(_pypowsybl.load_crac_source_with_parameters(network._handle, crac_source.getbuffer(), crac_file_name, parameters_source.getbuffer()))
 
     def get_instants(self) -> DataFrame:
