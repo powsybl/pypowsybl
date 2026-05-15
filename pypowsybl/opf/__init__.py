05c1af553524076dedab98b2122acebec1894add
Hugo KULESZA <hugo.kulesza@rte-france.com>
2026-05-15 11:05:48 +0200

    adding licenses + fix python sonar ci
    Signed-off-by: Hugo KULESZA <hugo.kulesza@rte-france.com>

--- pypowsybl/opf/__init__.py
+++ pypowsybl/opf/__init__.py
@@ -1,3 +1,10 @@
+#
+# Copyright (c) 2026, RTE (http://www.rte-france.com)
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+# SPDX-License-Identifier: MPL-2.0
+#
 from .impl.opf import OptimalPowerFlow, run_ac
 from .impl import bounds, constraints, costs, model
 
