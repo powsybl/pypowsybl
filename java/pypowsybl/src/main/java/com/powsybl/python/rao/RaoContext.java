b26f292b4380df4e04df2e27e112adbb535e4167
Bertrand Rix <bertrand.rix@artelys.com>
2026-06-09 16:07:55 +0200

    Marmot python API
    Signed-off-by: Bertrand Rix <bertrand.rix@artelys.com>

--- java/pypowsybl/src/main/java/com/powsybl/python/rao/RaoContext.java
+++ java/pypowsybl/src/main/java/com/powsybl/python/rao/RaoContext.java
@@ -11,8 +11,10 @@ import com.powsybl.commons.PowsyblException;
 import com.powsybl.glsk.api.GlskDocument;
 import com.powsybl.iidm.network.Network;
 import com.powsybl.loadflow.LoadFlowParameters;
+import com.powsybl.openrao.commons.TemporalDataImpl;
 import com.powsybl.openrao.data.crac.api.Crac;
 import com.powsybl.openrao.data.raoresult.api.RaoResult;
+import com.powsybl.openrao.data.timecoupledconstraints.TimeCoupledConstraints;
 import com.powsybl.openrao.monitoring.Monitoring;
 import com.powsybl.openrao.monitoring.MonitoringInput;
 import com.powsybl.openrao.monitoring.results.MonitoringResult;
@@ -20,7 +22,14 @@ import com.powsybl.openrao.monitoring.results.RaoResultWithAngleMonitoring;
 import com.powsybl.openrao.monitoring.results.RaoResultWithVoltageMonitoring;
 import com.powsybl.openrao.raoapi.Rao;
 import com.powsybl.openrao.raoapi.RaoInput;
+import com.powsybl.openrao.raoapi.TimeCoupledRaoInput;
 import com.powsybl.openrao.raoapi.parameters.RaoParameters;
+import com.powsybl.openrao.searchtreerao.marmot.Marmot;
+
+import java.time.OffsetDateTime;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @author Bertrand Rix {@literal <bertrand.rix at artelys.com>}
@@ -57,6 +66,15 @@ public class RaoContext {
             inputBuilder.withGlskProvider(loopFlowGlsk.getZonalGlsks(network));
         }
         return Rao.find(raoProvider).run(inputBuilder.build(), parameters);
+    }
+
+    public static RaoResult runMarmot(List<OffsetDateTime> timestamps, List<Network> networks, List<Crac> cracs, RaoParameters parameters, TimeCoupledConstraints timeCoupledConstraints) {
+        Map<OffsetDateTime, RaoInput> inputs = new HashMap<>();
+        for (int i = 0; i < timestamps.size(); ++i) {
+            inputs.put(timestamps.get(i), RaoInput.build(networks.get(i), cracs.get(i)).build());
+        }
+        TimeCoupledRaoInput timeCoupledInput = new TimeCoupledRaoInput(new TemporalDataImpl<>(inputs), timeCoupledConstraints);
+        return new Marmot().run(timeCoupledInput, parameters).join();
     }
 
     public RaoResultWithVoltageMonitoring runVoltageMonitoring(Network network, RaoResult resultIn, String provider, LoadFlowParameters parameters) {
