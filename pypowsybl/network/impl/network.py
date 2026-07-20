e3d11ddeef57c26c777c4814b01ea69df90b6cb8
HugoKulesza <hugo.kulesza@rte-france.com>
2026-07-20 16:38:37 +0200

    Miscellaneous documentation changes  (#1148)
    Signed-off-by: Hugo KULESZA <hugo.kulesza@rte-france.com>

--- pypowsybl/network/impl/network.py
+++ pypowsybl/network/impl/network.py
@@ -6742,8 +6742,9 @@ class Network:  # pylint: disable=too-many-public-methods
 
             .. code-block:: python
 
-                >>> properties_df = pd.Dataframe(index=pd.Series('id', ['G1', 'G2']),
+                >>> properties_df = pd.DataFrame.from_records(index='id',
                                                  data={
+                                                     'id': ['G1', 'G2'],
                                                      'prop1': [ 'val11', 'val12'],
                                                      'prop2': [ 'val12', 'val22'],
                                                  })
