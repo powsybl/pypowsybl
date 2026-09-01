289fdf3e2f632474bb5b2c1b3b23d9a512bf3f99
Landry Huet <landry.huet@supergrid-institute.com>
2026-09-01 15:38:32 +0200

    test: exercise droop curve segment mapper against a populated network

--- java/pypowsybl/src/test/java/com/powsybl/dataframe/network/NetworkDataframesTest.java
+++ java/pypowsybl/src/test/java/com/powsybl/dataframe/network/NetworkDataframesTest.java
@@ -1079,6 +1079,9 @@ class NetworkDataframesTest {
     @Test
     void droopCurveSegments() {
         Network network = DcDetailedNetworkFactory.createVscSymmetricalMonopole();
+        network.getVoltageSourceConverter("VscFr").newDroopCurve()
+                .beginSegment().setMinV(-500.0).setMaxV(-100.0).setK(-10.0).endSegment()
+                .add();
         List<Series> series = createDataFrame(DROOP_CURVE_SEGMENT, network);
 
         assertThat(series)
